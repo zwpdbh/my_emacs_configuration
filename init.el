fd20cd93fa1caa74d32420ef5df12e5c84fd9514
zwpdbh <hyperion_z@outlook.com>
2018-09-30 15:42:49 +0800

    remove not usually used plugins

---
+++ init.el
@@ -0,0 +1,34 @@
+(require 'cl)
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives
+	     '("gnu" . "http://elpa.gnu.org/packages/"))
+(add-to-list 'package-archives
+	     '("org" . "http://orgmode.org/elpa/"))
+(package-initialize)
+
+;; Bootstrap `use-package'
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+
+;; Use my init file in org-mode to set other packages
+(org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-directory "~/OneDrive/notes")
+ '(package-selected-packages
+   (quote
+    (treemacs-projectile treemacs-evil treemacs ess yasnippet-snippets which-key use-package try tabbar spacemacs-theme smartparens slime-company org-ref org-bullets org-ac irony-eldoc indent-guide flycheck exec-path-from-shell elisp-slime-nav counsel company-rtags company-math company-irony-c-headers company-irony cmake-mode auctex ace-window))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
