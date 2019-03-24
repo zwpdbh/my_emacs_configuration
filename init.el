de86521ae46dad32e3af671b98a3e708584e4205
zwpdbh <hyperion_z@outlook.com>
2019-03-24 22:19:34 +0800

    >speedup

--- init.el
+++ init.el
@@ -14,19 +14,18 @@
   (package-refresh-contents)
   (package-install 'use-package))
 
-
+(org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
 ;; Use my init file in org-mode to set other packages
 ;;(org-babel-load-file (expand-file-name "~/.emacs.d/myinit_for_windows.org"))
-(cond
- ((string-equal system-type "windows-nt")
-  (progn
-    (org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))))
- ((string-equal system-type "darwin")
-  (progn
-    (org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))))
- ((string-equal system-type "gnu/linux")
-  (progn
-    (org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org")))))
+
+;; Use a hook so the message doesn't get clobbered by other messages.
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (message "Emacs ready in %s with %d garbage collections."
+                     (format "%.2f seconds"
+                             (float-time
+                              (time-subtract after-init-time before-init-time)))
+                     gcs-done)))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -38,7 +37,7 @@
     ("028de01489a683696c64dcc2a01eaa663670d04202de3fce48ec3a5542bc2da5" "732b807b0543855541743429c9979ebfb363e27ec91e82f463c91e68c772f6e3" default)))
  '(package-selected-packages
    (quote
-    (typescript treemacs-magit treemacs-icons-dired auto-package-update dirtree slime-company json-mode xref-js2 js-refactor js2-mode flycheck-yamllint nlinum htmlize emacs-htmlize yaml-mode treemacs-projectile treemacs-evil treemacs ac-slime racket-mode slime macrostep elisp-slime-nav ensime which-key use-package try paredit material-theme leuven-theme indent-guide exec-path-from-shell counsel auto-complete ace-window))))
+    (slime-autoloads typescript treemacs-magit treemacs-icons-dired auto-package-update dirtree slime-company json-mode xref-js2 js-refactor js2-mode flycheck-yamllint nlinum htmlize emacs-htmlize yaml-mode treemacs-projectile treemacs-evil treemacs ac-slime racket-mode slime macrostep elisp-slime-nav ensime which-key use-package try paredit material-theme leuven-theme indent-guide exec-path-from-shell counsel auto-complete ace-window))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
