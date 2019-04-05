042b0a2016e5a44d3289b31c15876a8da4ece8a1
zwpdbh <hyperion_z@outlook.com>
2019-04-06 01:11:07 +0800

    >

--- init.el
+++ init.el
@@ -2,11 +2,7 @@
 (require 'package)
 (setq package-enable-at-startup nil)
 
-;; setup repositry in China for speed
-;; (setq package-archives '(("gnu"   . "http://elpa.emacs-china.org/gnu/")
-;;                          ("melpa-stable" . "http://elpa.emacs-china.org/melpa-stable/")
-;;                          ("melpa" . "http://elpa.emacs-china.org/melpa/")
-;;                          ("marmalada" . "http://elpa.emacs-china.org/marmalade/")))
+
 
 (add-to-list 'package-archives
 	     '("melpa" . "https://melpa.org/packages/"))
@@ -14,6 +10,8 @@
 	     '("gnu" . "http://elpa.gnu.org/packages/"))
 (add-to-list 'package-archives
 	     '("org" . "http://orgmode.org/elpa/"))
+(add-to-list 'package-archives
+	     '("melpa-china" . "http://elpa.emacs-china.org/melpa/"))
 
 
 (package-initialize)
@@ -44,7 +42,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (jedi helm-cider helm aggressive-indent rainbow-delimiters cider base16-theme gruvbox-theme zenburn-theme yaml-mode xref-js2 which-key web-mode use-package try treemacs-projectile treemacs-magit treemacs-icons-dired treemacs-evil tide spacemacs-theme solarized-theme slime-company racket-mode paredit nlinum neotree material-theme leuven-theme json-mode js2-refactor indent-tools indent-guide htmlize flycheck-yamllint exec-path-from-shell esup ensime elpy elisp-slime-nav dirtree counsel company-tern autopair auto-package-update adaptive-wrap ac-slime))))
+    (ivy-hydra jedi helm-cider helm aggressive-indent rainbow-delimiters cider base16-theme gruvbox-theme zenburn-theme yaml-mode xref-js2 which-key web-mode use-package try treemacs-projectile treemacs-magit treemacs-icons-dired treemacs-evil tide spacemacs-theme solarized-theme slime-company racket-mode paredit nlinum neotree material-theme leuven-theme json-mode js2-refactor indent-tools indent-guide htmlize flycheck-yamllint exec-path-from-shell esup ensime elpy elisp-slime-nav dirtree counsel company-tern autopair auto-package-update adaptive-wrap ac-slime))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
