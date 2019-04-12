18082d4c79ee8155fe06d257810830f8d7e7d3cf
Zhao, Wei <frank.zhao@siemens.com>
2019-04-12 10:59:01 +0800

    add elpa to use org-mode-contrib and set sly when evaluating lisp code in org-mode

--- init.el
+++ init.el
@@ -3,7 +3,8 @@
 (setq package-enable-at-startup nil)
 
 
-
+(add-to-list 'package-archives 
+	     '("org" . "http://orgmode.org/elpa/") t)
 (add-to-list 'package-archives
 	     '("melpa" . "https://melpa.org/packages/"))
 (add-to-list 'package-archives
