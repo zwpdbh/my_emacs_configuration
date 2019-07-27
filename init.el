ee305416438622c8d922a65a25a70130137b8199
zwpdbh <hyperion_z@outlook.com>
2019-07-27 13:25:31 +0800

    <

--- init.el
+++ init.el
@@ -35,9 +35,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-agenda-files
-   (quote
-    ("~/.emacs.d/myinit.org" "~/code/org/emacs.org" "/home/zw/code/org/kubernetes/use-kubernetes-api.org" "/home/zw/code/org/blog-creation.org" "/home/zw/code/org/chez-scheme.org" "/home/zw/code/org/clojure.org" "/home/zw/code/org/learning_statistics_using_R.org" "/home/zw/code/org/personal.org")))
+ '(org-agenda-files (quote ("~/.emacs.d/myinit.org" "~/code/org/emacs.org")))
  '(package-selected-packages
    (quote
     (ob-http spacemacs-theme docker-tramp docker-compose-mode dockerfile-mode treemacs-icons-dired treemacs-projectile treemacs-evil treemacs ox-gfm htmlize yaml-mode which-key use-package try smartparens sly rainbow-delimiters racket-mode paredit nlinum monokai-theme magit lsp-ui lsp-python-ms lispy json-mode indent-guide highlight-indentation helm-xref helm-projectile helm-cider gruvbox-theme geiser flycheck-yamllint expand-region exec-path-from-shell ess ensime dap-mode cquery company-statistics company-quickhelp company-lsp cmake-mode clang-format aggressive-indent adoc-mode adaptive-wrap))))
