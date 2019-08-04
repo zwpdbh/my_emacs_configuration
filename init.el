852b32ae8d0f94ec2e504541c7ac8ff609b0c589
zwpdbh <hyperion_z@outlook.com>
2019-08-04 16:10:52 +0800

    > latex

--- init.el
+++ init.el
@@ -39,7 +39,7 @@
     ("~/code/org/clojure.org" "~/code/org/job.org" "~/.emacs.d/myinit.org" "~/code/org/emacs.org")))
  '(package-selected-packages
    (quote
-    (pdf-tools dakrone-theme tangotango-theme color-theme-sanityinc-tomorrow ample-theme airline-themes base16-theme vue-mode org-tempo ob-http spacemacs-theme docker-tramp docker-compose-mode dockerfile-mode treemacs-icons-dired treemacs-projectile treemacs-evil treemacs ox-gfm htmlize yaml-mode which-key use-package try smartparens sly rainbow-delimiters racket-mode paredit nlinum monokai-theme magit lsp-ui lsp-python-ms lispy json-mode indent-guide highlight-indentation helm-xref helm-projectile helm-cider gruvbox-theme geiser flycheck-yamllint expand-region exec-path-from-shell ess ensime dap-mode cquery company-statistics company-quickhelp company-lsp cmake-mode clang-format aggressive-indent adoc-mode adaptive-wrap))))
+    (auctex company-math pdf-tools dakrone-theme tangotango-theme color-theme-sanityinc-tomorrow ample-theme airline-themes base16-theme vue-mode org-tempo ob-http spacemacs-theme docker-tramp docker-compose-mode dockerfile-mode treemacs-icons-dired treemacs-projectile treemacs-evil treemacs ox-gfm htmlize yaml-mode which-key use-package try smartparens sly rainbow-delimiters racket-mode paredit nlinum monokai-theme magit lsp-ui lsp-python-ms lispy json-mode indent-guide highlight-indentation helm-xref helm-projectile helm-cider gruvbox-theme geiser flycheck-yamllint expand-region exec-path-from-shell ess ensime dap-mode cquery company-statistics company-quickhelp company-lsp cmake-mode clang-format aggressive-indent adoc-mode adaptive-wrap))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
