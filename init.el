085321fdec6e282e979c8c22bfa4fcf36a90a955
Zhao, Wei <frank.zhao@siemens.com>
2019-11-22 21:37:58 +0800

    <<

--- init.el
+++ init.el
@@ -49,3 +49,4 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
+(put 'narrow-to-region 'disabled nil)
