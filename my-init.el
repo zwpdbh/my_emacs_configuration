42997b177a2939c1f3de4dbaad36696e56658a28
Zhao, Wei <frank.zhao@siemens.com>
2019-12-18 16:06:01 +0800

    <<

--- my-init.el
+++ my-init.el
@@ -1,20 +1,47 @@
-;; (require 'cl)
-(require 'package)
-(setq package-enable-at-startup nil)
-
-(setq package-archives '(("gnu-qinghua"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
-			 ("melpa-qinghua" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")
-			 ("org-qinghua"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/org/")))
-
 (setq my-emacs-root-directory "~/.emacs.d/")
 (add-to-list 'load-path (expand-file-name "lisp" my-emacs-root-directory))
-(require 'init-benchmarking) ;; Measure startup time
-(package-initialize)
+(require 'init-benchmarking) 
 
-;; Bootstrap `use-package'
+(defconst *spell-check-support-enabled* nil) ;; Enable with t if you prefer
+(defconst *is-a-mac* (eq system-type 'darwin))
+
+;;----------------------------------------------------------------------------
+;; Adjust garbage collection thresholds during startup, and thereafter
+;;----------------------------------------------------------------------------
+(let ((normal-gc-cons-threshold (* 20 1024 1024))
+      (init-gc-cons-threshold (* 128 1024 1024)))
+  (setq gc-cons-threshold init-gc-cons-threshold)
+  (add-hook 'emacs-startup-hook
+            (lambda () (setq gc-cons-threshold normal-gc-cons-threshold))))
+
+;;----------------------------------------------------------------------------
+;; Bootstrap config
+;;----------------------------------------------------------------------------
+(setq custom-file (expand-file-name "custom.el" my-emacs-root-directory))
+(require 'init-utils)
+;; (require 'init-site-lisp) ;; Must come before elpa, as it may provide package.el
+;; Calls (package-initialize)
+(require 'init-elpa)      ;; Machinery for installing required packages
+(require 'init-exec-path) ;; Set up $PATH
+
+;;----------------------------------------------------------------------------
+;; Allow users to provide an optional "init-preload-local.el"
+;;----------------------------------------------------------------------------
+(require 'init-preload-local nil t)
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
 
-;; Use my init file in org-mode to set other packages
+
+;;----------------------------------------------------------------------------
+;; Load configs for specific features and modes
+;;----------------------------------------------------------------------------
+(require-package 'diminish)
+(maybe-require-package 'scratch)
+(require-package 'command-log-mode)
+
+(require 'init-frame-hooks)
+(require 'init-xterm)
+(require 'init-gui-frames)
+
 (org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
