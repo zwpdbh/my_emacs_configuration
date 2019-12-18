02b67cf7b609e133d2209ed5c6da9814f20147f7
zwpdbh <hyperion_z@outlook.com>
2019-12-18 22:25:42 +0800

    <<<

--- my-init.el
+++ my-init.el
@@ -84,7 +84,7 @@
 ;; (require 'init-markdown)
 ;; (require 'init-csv)
 ;; (require 'init-erlang)
-;; (require 'init-javascript)
+(require 'init-javascript)
 ;; (require 'init-php)
 ;; (require 'init-org)
 ;; (require 'init-nxml)
