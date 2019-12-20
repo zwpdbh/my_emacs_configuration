78e2c1343706142928143f9681428d0860e4306a
Zhao, Wei <frank.zhao@siemens.com>
2019-12-20 09:21:40 +0800

    <

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -5,6 +5,10 @@
 ;; (require-package 'color-theme-sanityinc-solarized)
 ;; (require-package 'color-theme-sanityinc-tomorrow)
 
+;; Don't prompt to confirm theme safety. This avoids problems with
+;; first-time startup on Emacs > 26.3.
+(setq custom-safe-themes t)
+
 (use-package doom-themes
   :init
   (setq doom-themes-enable-bold nil
@@ -12,17 +16,40 @@
   :ensure t
   :config
   (progn
-    (load-theme 'doom-vibrant t)
-    (setq doom-themes-treemacs-theme "doom-colors") ; use the colorful treemacs theme
+    (load-theme 'doom-solarized-light t)
+    
+    ;; use the colorful treemacs theme
+    (setq doom-themes-treemacs-theme "doom-colors") 
     ;; (doom-themes-treemacs-config)
+    
     ;; Corrects (and improves) org-mode's native fontification.
     ;; (doom-themes-org-config)
+    
     (set-cursor-color "HotPink")
-    (setq-default cursor-type '(bar . 2))))
+    (setq-default cursor-type '(bar . 2))
+    (set-face-attribute 'show-paren-match nil
+                        :underline t
+                        :foreground "SteelBlue"
+                        :background nil
+                        :weight 'ultrabold)))
 
-;; Don't prompt to confirm theme safety. This avoids problems with
-;; first-time startup on Emacs > 26.3.
-(setq custom-safe-themes t)
+;; (use-package gruvbox-theme
+;;   :init
+;;   :ensure t
+;;   :config
+;;   (progn
+;;     (load-theme 'gruvbox-light-hard t)
+;;     ;; (set-cursor-color "HotPink")
+;;     (setq-default cursor-type '(bar . 2))))
+
+;; (use-package solarized-theme
+;;   :init
+;;   :ensure t
+;;   :config
+;;   (progn
+;;     (load-theme 'solarized-gruvbox-light t)
+;;     ;; (set-cursor-color "HotPink")
+;;     (setq-default cursor-type '(bar . 2))))
 
 
 ;; Ensure that themes will be applied even if they have not been customized
