97dcc07410f3f1e2669db6c4f41a3f7721f38118
Zhao, Wei <frank.zhao@siemens.com>
2020-01-15 18:00:09 +0800

    << change theme

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -1,69 +1,54 @@
 ;;; init-themes.el --- Defaults for themes -*- lexical-binding: t -*-
-;;; Commentary:
-;;; Code:
 
-;; (require-package 'color-theme-sanityinc-solarized)
-;; (require-package 'color-theme-sanityinc-tomorrow)
-
-;; Don't prompt to confirm theme safety. This avoids problems with
-;; first-time startup on Emacs > 26.3.
-(setq custom-safe-themes t)
-
-(use-package doom-themes
-  :init
-  (setq doom-themes-enable-bold nil
-        doom-themes-enable-italic nil)
-  :ensure t
-  :config
-  (progn
-    (load-theme 'doom-tomorrow-night t)
-    
-    ;; use the colorful treemacs theme
-    (setq doom-themes-treemacs-theme "doom-colors")
-    ;; (doom-themes-treemacs-config)
-    
-    ;; Corrects (and improves) org-mode's native fontification.
-    ;; (doom-themes-org-config)
-    
-    (set-cursor-color "HotPink")
-    (setq-default cursor-type '(bar . 2))
-    (set-face-attribute 'show-paren-match nil
-                        :underline t
-                        :foreground "SteelBlue"
-                        :background nil
-                        :weight 'ultrabold)))
-
-;; (use-package gruvbox-theme
+;; (use-package doom-themes
 ;;   :init
+;;   (setq custom-safe-themes t)
+;;   (setq doom-themes-enable-bold nil
+;;         doom-themes-enable-italic nil)
 ;;   :ensure t
 ;;   :config
 ;;   (progn
-;;     (load-theme 'gruvbox-light-hard t)
-;;     ;; (set-cursor-color "HotPink")
-;;     (setq-default cursor-type '(bar . 2))))
+;;     (load-theme 'doom-tomorrow-night t)
 
-;; (use-package solarized-theme
-;;   :init
-;;   :ensure t
-;;   :config
-;;   (progn
-;;     (load-theme 'solarized-gruvbox-light t)
-;;     ;; (set-cursor-color "HotPink")
-;;     (setq-default cursor-type '(bar . 2))))
+;;     ;; use the colorful treemacs theme
+;;     (setq doom-themes-treemacs-theme "doom-colors")
+;;     ;; (doom-themes-treemacs-config)
 
+;;     ;; Corrects (and improves) org-mode's native fontification.
+;;     ;; (doom-themes-org-config)
+
+;;     (set-cursor-color "HotPink")
+;;     (setq-default cursor-type '(bar . 2))
+;;     (set-face-attribute 'show-paren-match nil
+;;                         :underline t
+;;                         :foreground "SteelBlue"
+;;                         :background nil
+;;                         :weight 'ultrabold)))
+
+(use-package color-theme-sanityinc-tomorrow
+  :ensure t)
+
+;; If you don't customize it, this is the theme you get.
+(setq-default custom-enabled-themes '(sanityinc-tomorrow-bright))
 
 ;; Ensure that themes will be applied even if they have not been customized
 (defun reapply-themes ()
   "Forcibly load the themes listed in `custom-enabled-themes'."
+  (setq custom-safe-themes t)
   (dolist (theme custom-enabled-themes)
     (unless (custom-theme-p theme)
       (load-theme theme)))
-  (custom-set-variables `(custom-enabled-themes (quote ,custom-enabled-themes))))
+  (custom-set-variables `(custom-enabled-themes (quote ,custom-enabled-themes)))
+  
+  (set-cursor-color "HotPink")
+  (setq-default cursor-type '(bar . 2))
+  (set-face-attribute 'show-paren-match nil
+                      :underline t
+                      :foreground "SteelBlue";; "#D4FF00"
+                      :background nil
+                      :weight 'ultrabold))
 
-;; If you don't customize it, this is the theme you get.
-;; (setq-default custom-enabled-themes '(doom-vibrant))
-;; (add-hook 'after-init-hook 'reapply-themes)
-
+(add-hook 'after-init-hook 'reapply-themes)
 
 ;;------------------------------------------------------------------------------
 ;; Toggle between light and dark
@@ -71,13 +56,13 @@
 (defun light ()
   "Activate a light color theme."
   (interactive)
-  (setq custom-enabled-themes '(doom-one-light))
+  (setq custom-enabled-themes '(sanityinc-tomorrow-day))
   (reapply-themes))
 
 (defun dark ()
   "Activate a dark color theme."
   (interactive)
-  (setq custom-enabled-themes '(doom-vibrant))
+  (setq custom-enabled-themes '(sanityinc-tomorrow-bright))
   (reapply-themes))
 
 (provide 'init-themes)
