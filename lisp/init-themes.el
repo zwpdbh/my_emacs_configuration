36c23609cc1d059775025615797f237836059c87
zwpdbh <hyperion_z@outlook.com>
2020-02-19 13:31:59 +0800

    << update-theme

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -33,18 +33,15 @@
   :defer t)
 
 ;; set default theme
-(setq-default custom-enabled-themes '(sanityinc-tomorrow-bright))
-;; (setq-default custom-enabled-themes '(sanityinc-tomorrow-night))
+;; sanityinc-tomorrow-night
+;; sanityinc-tomorrow-bright
+(setq-default custom-enabled-theme 'sanityinc-tomorrow-night)
 
 ;; Ensure that themes will be applied even if they have not been customized
 (defun reapply-themes ()
   "Forcibly load the themes listed in `custom-enabled-themes'."
-  (setq custom-safe-themes t) ; Don't prompt to confirm theme safety. This avoids problems with
-  (dolist (theme custom-enabled-themes)
-    (unless (custom-theme-p theme)
-      (load-theme theme)))
-  
-  (custom-set-variables `(custom-enabled-themes (quote ,custom-enabled-themes)))
+  (setq custom-safe-themes t) ; Don't prompt to confirm theme safety. This 
+  (load-theme custom-enabled-theme)
   
   (set-cursor-color "HotPink")
   (setq-default cursor-type '(bar . 2))
