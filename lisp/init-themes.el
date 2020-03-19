56ed56ace93a628830b709bcbbc663ada7d97629
zwpdbh <hyperion_z@outlook.com>
2020-03-19 18:35:41 +0800

    <<< done theme

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -29,10 +29,10 @@
 
 (defun zw/customize-theme ()
   (load-theme custom-enabled-theme)
-  
-  (set-cursor-color "#73c936")
-  ;; (setq-default cursor-type '(bar . 3))
-  (setq-default cursor-type 'hollow)
+
+  (set-cursor-color "IndianRed")
+  (setq-default cursor-type '(bar . 2))
+  ;; (setq-default cursor-type 'hollow)
   
   (setq show-paren-style 'expression)
   ;; tried color candidates: SteelBlue
