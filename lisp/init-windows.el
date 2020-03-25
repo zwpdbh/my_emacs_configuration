d1c1d116277b618e92cc97656c267636edd31f6f
Zhao, Wei <frank.zhao@siemens.com>
2020-03-25 17:06:14 +0800

    < toggle full window and other window

--- lisp/init-windows.el
+++ lisp/init-windows.el
@@ -1,105 +1,165 @@
-;;; init-windows.el --- Working with windows within frames -*- lexical-binding: t -*-
-;;; Commentary:
+;; -*- coding: utf-8; lexical-binding: t; -*-
 
-;; This is not about the "Windows" OS, but rather Emacs's "windows"
-;; concept: these are the panels within an Emacs frame which contain
-;; buffers.
-
-;;; Code:
-
-;;----------------------------------------------------------------------------
 ;; Navigate window layouts with "C-c <left>" and "C-c <right>"
-;;----------------------------------------------------------------------------
-(add-hook 'after-init-hook 'winner-mode)
+(winner-mode 1)
+;; copied from http://puntoblogspot.blogspot.com/2011/05/undo-layouts-in-emacs.html
+(global-set-key (kbd "C-x 4 u") 'winner-undo)
+(global-set-key (kbd "C-x 4 U") 'winner-redo)
 
-;; Make "C-x o" prompt for a target window when there are more than 2
-(require-package 'switch-window)
-(setq-default switch-window-shortcut-style 'alphabet)
-(setq-default switch-window-timeout nil)
-(global-set-key (kbd "C-x o") 'switch-window)
+(defvar my-ratio-dict
+  '((1 . 1.61803398875)
+    (2 . 2)
+    (3 . 3)
+    (4 . 4)
+    (5 . 0.61803398875))
+  "The ratio dictionary.")
+
+(defun my-split-window-horizontally (&optional ratio)
+  "Split window horizontally and resize the new window.
+'C-u number M-x my-split-window-horizontally' uses pre-defined
+ratio from `my-ratio-dict'.
+Always focus on bigger window."
+  (interactive "P")
+  (let* (ratio-val)
+    (cond
+     (ratio
+      (setq ratio-val (cdr (assoc ratio my-ratio-dict)))
+      (split-window-horizontally (floor (/ (window-body-width)
+                                           (1+ ratio-val)))))
+     (t
+      (split-window-horizontally)))
+    (set-window-buffer (next-window) (current-buffer))
+    (if (or (not ratio-val)
+            (>= ratio-val 1))
+        (windmove-right))))
+
+(defun my-split-window-vertically (&optional ratio)
+  "Split window vertically and resize the new window.
+'C-u number M-x my-split-window-vertically' uses pre-defined
+ratio from `my-ratio-dict'.
+Always focus on bigger window."
+  (interactive "P")
+  (let* (ratio-val)
+    (cond
+     (ratio
+      (setq ratio-val (cdr (assoc ratio my-ratio-dict)))
+      (split-window-vertically (floor (/ (window-body-height)
+                                         (1+ ratio-val)))))
+     (t
+      (split-window-vertically)))
+    ;; open another window with current-buffer
+    (set-window-buffer (next-window) (current-buffer))
+    ;; move focus if new window bigger than current one
+    (if (or (not ratio-val)
+            (>= ratio-val 1))
+        (windmove-down))))
+
+(global-set-key (kbd "C-x 2") 'my-split-window-vertically)
+(global-set-key (kbd "C-x 3") 'my-split-window-horizontally)
 
 
-;;----------------------------------------------------------------------------
-;; When splitting window, show (other-buffer) in the new window
-;;----------------------------------------------------------------------------
-(defun split-window-func-with-other-buffer (split-function)
-  (lambda (&optional arg)
-    "Split this window and switch to the new window unless ARG is provided."
-    (interactive "P")
-    (funcall split-function)
-    (let ((target-window (next-window)))
-      (set-window-buffer target-window (other-buffer))
-      (unless arg
-        (select-window target-window)))))
-
-(global-set-key (kbd "C-x 2") (split-window-func-with-other-buffer 'split-window-vertically))
-(global-set-key (kbd "C-x 3") (split-window-func-with-other-buffer 'split-window-horizontally))
-
-(defun sanityinc/toggle-delete-other-windows ()
-  "Delete other windows in frame if any, or restore previous window config."
+(defun scroll-other-window-up ()
   (interactive)
-  (if (and winner-mode
-           (equal (selected-window) (next-window)))
-      (winner-undo)
-    (delete-other-windows)))
+  (scroll-other-window '-))
 
-(global-set-key (kbd "C-x 1") 'sanityinc/toggle-delete-other-windows)
-
-;;----------------------------------------------------------------------------
-;; Rearrange split windows
-;;----------------------------------------------------------------------------
-(defun split-window-horizontally-instead ()
-  "Kill any other windows and re-split such that the current window is on the top half of the frame."
+(defun toggle-two-split-window ()
   (interactive)
-  (let ((other-buffer (and (next-window) (window-buffer (next-window)))))
-    (delete-other-windows)
-    (split-window-horizontally)
-    (when other-buffer
-      (set-window-buffer (next-window) other-buffer))))
+  (when (= (count-windows) 2)
+    (let* ((this-win-buffer (window-buffer))
+           (next-win-buffer (window-buffer (next-window)))
+           (this-win-edges (window-edges (selected-window)))
+           (next-win-edges (window-edges (next-window)))
+           (this-win-2nd (not (and (<= (car this-win-edges)
+                                       (car next-win-edges))
+                                   (<= (cadr this-win-edges)
+                                       (cadr next-win-edges)))))
+           (splitter
+            (if (= (car this-win-edges)
+                   (car (window-edges (next-window))))
+                'split-window-horizontally
+              'split-window-vertically)))
+      (delete-other-windows)
+      (let* ((first-win (selected-window)))
+        (funcall splitter)
+        (if this-win-2nd (other-window 1))
+        (set-window-buffer (selected-window) this-win-buffer)
+        (set-window-buffer (next-window) next-win-buffer)
+        (select-window first-win)
+        (if this-win-2nd (other-window 1))))))
 
-(defun split-window-vertically-instead ()
-  "Kill any other windows and re-split such that the current window is on the left half of the frame."
+(defun rotate-windows ()
+  "Rotate windows in clock-wise direction."
   (interactive)
-  (let ((other-buffer (and (next-window) (window-buffer (next-window)))))
-    (delete-other-windows)
-    (split-window-vertically)
-    (when other-buffer
-      (set-window-buffer (next-window) other-buffer))))
+  (cond ((not (> (count-windows)1))
+         (message "You can't rotate a single window!"))
+        (t
+         (setq i 1)
+         (setq numWindows (count-windows))
+         (while (< i numWindows)
+           (let* (
+                  (w1 (elt (window-list) i))
+                  (w2 (elt (window-list) (+ (% i numWindows) 1)))
 
-(global-set-key (kbd "C-x |") 'split-window-horizontally-instead)
-(global-set-key (kbd "C-x _") 'split-window-vertically-instead)
+                  (b1 (window-buffer w1))
+                  (b2 (window-buffer w2))
 
-;; Borrowed from http://postmomentum.ch/blog/201304/blog-on-emacs
-(defun sanityinc/split-window()
-  "Split the window to see the most recent buffer in the other window.
-Call a second time to restore the original window configuration."
+                  (s1 (window-start w1))
+                  (s2 (window-start w2))
+                  )
+             (set-window-buffer w1 b2)
+             (set-window-buffer w2 b1)
+             (set-window-start w1 s2)
+             (set-window-start w2 s1)
+             (setq i (1+ i)))))))
+
+;; https://github.com/abo-abo/ace-window
+;; `M-x ace-window ENTER m` to swap window
+(global-set-key (kbd "C-x o") 'ace-window)
+
+;; {{ move focus between sub-windows
+(setq winum-keymap
+      (let ((map (make-sparse-keymap)))
+        (define-key map (kbd "M-0") 'winum-select-window-0-or-10)
+        (define-key map (kbd "M-1") 'winum-select-window-1)
+        (define-key map (kbd "M-2") 'winum-select-window-2)
+        (define-key map (kbd "M-3") 'winum-select-window-3)
+        (define-key map (kbd "M-4") 'winum-select-window-4)
+        (define-key map (kbd "M-5") 'winum-select-window-5)
+        (define-key map (kbd "M-6") 'winum-select-window-6)
+        (define-key map (kbd "M-7") 'winum-select-window-7)
+        (define-key map (kbd "M-8") 'winum-select-window-8)
+        map))
+
+(use-package winum
+  :ensure t)
+
+(eval-after-load 'winum
+  '(progn
+     (setq winum-format "%s")
+     (setq winum-mode-line-position 0)
+     (set-face-attribute 'winum-face nil :foreground "DeepPink" :underline "DeepPink" :weight 'bold)
+     (winum-mode 1)))
+;; }}
+
+(defun toggle-full-window()
+  "Toggle full view of selected window."
   (interactive)
-  (if (eq last-command 'sanityinc/split-window)
-      (progn
-        (jump-to-register :sanityinc/split-window)
-        (setq this-command 'sanityinc/unsplit-window))
-    (window-configuration-to-register :sanityinc/split-window)
-    (switch-to-buffer-other-window nil)))
+  ;; @see http://www.gnu.org/software/emacs/manual/html_node/elisp/Splitting-Windows.html
+  (if (window-parent)
+      (delete-other-windows)
+    (winner-undo)))
 
-(global-set-key (kbd "<f7>") 'sanityinc/split-window)
-
-
-(defun sanityinc/toggle-current-window-dedication ()
-  "Toggle whether the current window is dedicated to its current buffer."
-  (interactive)
-  (let* ((window (selected-window))
-         (was-dedicated (window-dedicated-p window)))
-    (set-window-dedicated-p window (not was-dedicated))
-    (message "Window %sdedicated to %s"
-             (if was-dedicated "no longer " "")
-             (buffer-name))))
-
-(global-set-key (kbd "C-c <down>") 'sanityinc/toggle-current-window-dedication)
-
-
-(unless (memq window-system '(nt w32))
-  (windmove-default-keybindings 'control))
-
+(use-package ace-window
+  :defer 2
+  :ensure t
+  :init
+  :config
+  (progn
+    (setq aw-scope 'frame)
+    (global-set-key (kbd "C-x O") 'other-frame)
+    (global-set-key [remap other-window] 'ace-window)
+    (custom-set-faces
+     '(aw-leading-char-face
+       ((t (:inherit ace-jump-face-foreground :height 3.0)))))))
 
 (provide 'init-windows)
-;;; init-windows.el ends here
