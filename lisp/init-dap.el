fb78f93474c7831a01bf2f1ae6777d389bf4521b
zwpdbh <hyperion_z@outlook.com>
2020-03-31 23:43:38 +0800

    <<< speed up

--- lisp/init-dap.el
+++ lisp/init-dap.el
@@ -1,25 +1,20 @@
-(use-package dap-mode
-  :ensure t
-  :config
-  (progn
-    (dap-mode 1)
-    (dap-ui-mode 1)
-    ;; (dap-tooltip-mode 1)
-    ;; (setq tooltip-mode t)
+(when (maybe-require-package 'dap-mode)
+  ;; dap-mode also provides a hydra with dap-hydra
+  (add-hook 'dap-stopped-hook
+            (lambda (arg) (call-interactively #'dap-hydra)))
 
-    ;; dap-mode also provides a hydra with dap-hydra
-    (add-hook 'dap-stopped-hook
-              (lambda (arg) (call-interactively #'dap-hydra)))
-
-    ;; for javascript node debug 
-    (require 'dap-node)
-    (dap-register-debug-template
-     "Node::zwpdbh-debug"
-     (list :type "node"
-           :cwd nil
-           :request "launch"
-           :program nil
-           :name "Node::zwpdbh-debug"))))
+  ;; for javascript node debug 
+  (require 'dap-node)
+  (dap-register-debug-template
+   "Node::zwpdbh-debug"
+   (list :type "node"
+         :cwd nil
+         :request "launch"
+         :program nil
+         :name "Node::zwpdbh-debug"))
+  (add-hook 'dap-mode-hook '(lambda ()
+                              (dap-mode 1)
+                              (dap-ui-mode 1))))
 
 ;; Need to call ~dap-node-setup~ for setting up vscode extension.
 ;; Make sure the ~dap-node-debug-program~ is pointing to the proper file.
