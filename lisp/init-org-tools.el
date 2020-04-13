4668a3d46f99f110d5ab84f00bb7de7d8e76d8bf
zwpdbh <hyperion_z@outlook.com>
2020-04-13 14:10:14 +0800

    >>

--- lisp/init-org-tools.el
+++ lisp/init-org-tools.el
@@ -20,24 +20,23 @@
                             "_att")))
           org-attach-screenshot-relative-links t)))
 
-
-(defun zw/org-screenshot ()
-  "Take a screenshot into a time stamped unique-named file in the
+;; create org-screenshot function based on system
+(if *win64*
+    (defun zw/org-screenshot ()
+      "Take a screenshot into a time stamped unique-named file in the
      same directory as the org-buffer and insert a link to this file."
-  (interactive)
-  (if *win64*
-      (progn
-        (setq filename
-              (concat
-               (make-temp-name
-                (concat (file-name-directory buffer-file-name)
-                        "_"
-                        (format-time-string "%Y%m%d_%H%M%S_")) ) ".png"))
-        (shell-command "snippingtool /clip")
-        (shell-command (concat "powershell -command \"Add-Type -AssemblyName System.Windows.Forms;if ($([System.Windows.Forms.Clipboard]::ContainsImage())) {$image = [System.Windows.Forms.Clipboard]::GetImage();[System.Drawing.Bitmap]$image.Save('" filename "',[System.Drawing.Imaging.ImageFormat]::Png); Write-Output 'clipboard content saved as file'} else {Write-Output 'clipboard does not contain image data'}\""))
-        (insert (concat "[[file:" filename "]]"))
-        (org-display-inline-images))
-    #'org-attach-screenshot))
+      (interactive)
+      (setq filename
+            (concat
+             (make-temp-name
+              (concat (file-name-directory buffer-file-name)
+                      "_"
+                      (format-time-string "%Y%m%d_%H%M%S_")) ) ".png"))
+      (shell-command "snippingtool /clip")
+      (shell-command (concat "powershell -command \"Add-Type -AssemblyName System.Windows.Forms;if ($([System.Windows.Forms.Clipboard]::ContainsImage())) {$image = [System.Windows.Forms.Clipboard]::GetImage();[System.Drawing.Bitmap]$image.Save('" filename "',[System.Drawing.Imaging.ImageFormat]::Png); Write-Output 'clipboard content saved as file'} else {Write-Output 'clipboard does not contain image data'}\""))
+      (insert (concat "[[file:" filename "]]"))
+      (org-display-inline-images))
+  (defalias 'zw/org-screenshot 'org-attach-screenshot))
 
 (add-hook 'org-mode-hook '(lambda ()
                             (define-key org-mode-map (kbd "\C-c s") 'zw/org-screenshot)))
