8de4f57944d17814ee39d0e5d839574483b6f61c
zwpdbh <hyperion_z@outlook.com>
2020-06-04 23:56:27 +0800

    > Terminus TTF has issues with Chinese font, use source code pro

--- lisp/init-interface-tweaks.el
+++ lisp/init-interface-tweaks.el
@@ -2,7 +2,7 @@
 ;; https://github.com/adobe-fonts/source-code-pro
 ;; to adjust font dynamically
 ;; C-xC-+ and C-xC-- to increase or decrease the buffer text size
-(set-face-attribute 'default nil :height 120)
+(set-face-attribute 'default nil :height 110)
 
 ;; (require 'cl)
 ;; (defun font-candidate (&rest fonts)
@@ -16,13 +16,13 @@
 ;;                     :height 110)
 
 (cond
+ ((member "Source Code Pro" (font-family-list))
+  (set-frame-font "Source Code Pro"))
  ((and *win64* (member "Terminus (TTF) for Windows" (font-family-list)))
   (set-frame-font "Terminus (TTF) for Windows"))
  ((member "Terminus (TTF)" (font-family-list))
   (print "hello")
-  (set-frame-font "Terminus (TTF)"))
- ((member "Source Code Pro" (font-family-list))
-  (set-frame-font "Source Code Pro")))
+  (set-frame-font "Terminus (TTF)")))
 ;; check current font are using: http://ergoemacs.org/emacs/emacs_list_and_set_font.html
 ;; terminus font: https://files.ax86.net/terminus-ttf/#what
 
