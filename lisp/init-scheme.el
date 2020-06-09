4db789a720ed10600d2f78d4b33c6e3a3b8b7e0f
zwpdbh <hyperion_z@outlook.com>
2020-06-10 00:44:58 +0800

    <<

--- lisp/init-scheme.el
+++ lisp/init-scheme.el
@@ -24,57 +24,15 @@
          (setq geiser-chez-binary "chezscheme9.5")))
 
   ;; (setq geiser-active-implementations '(guile chez))
-  (setq geiser-default-implementation 'guile)
+  ;; (setq geiser-default-implementation 'guile)
   (setq geiser-active-implementations '(guile chez))
 
   (setq geiser-mode-start-repl-p t)
   (add-to-list 'auto-mode-alist '("\\.scheme\\'" . scheme-mode))
   ;; (add-to-list 'auto-mode-alist '("\\.racket\\'" . scheme-mode))
   ;; (add-to-list 'auto-mode-alist '("\\.rkt\\'" . scheme-mode))
-  
   (add-hook 'scheme-mode-hook 'geiser-mode)
-  (add-hook 'geiser-mode-hook
-            #'(lambda ()
-                (define-key geiser-mode-map (kbd "C-c C-c") 'geiser-eval-definition-and-go))))
-
-;; (use-package geiser
-;;   :defer t
-;;   :init
-;;   ;; append exec-path to include chez scheme
-;;   (cond ((eq system-type 'windows-nt)
-;;          (setq exec-path (append exec-path '("c:/Program Files (x86)/Chez Scheme 9.5/bin/ti3nt"))))
-;;         ((eq system-type 'darwin)
-;;          (setq exec-path (append exec-path '("/usr/local/bin"))))
-;;         ((eq system-type 'gnu/linux)
-;;          (setq exec-path (append exec-path '("/usr/bin")))))
-;;   ;; set Library directories
-;;   (cond ((eq system-type 'windows-nt)
-;;          (setenv "CHEZSCHEMELIBDIRS" "C:\\scheme\\lib;")
-;;          (setenv "CHEZSCHEMELIBEXTS" ".sc;;.so;"))
-;;         ((eq system-type 'darwin)
-;;          ;; raven is the chez scheme package management tool
-;;          (setenv "CHEZSCHEMELIBDIRS" "/usr/local/lib/raven")
-;;          (setenv "CHEZSCHEMELIBEXTS" ".sc::.so:"))
-;;         (t
-;;          nil))
-
-;;   (cond ((eq system-type 'darwin)
-;;          (setq geiser-chez-binary "chez"))
-;;         (*win64*
-;;          (setq geiser-chez-binary "scheme.exe"))
-;;         (t
-;;          (setq geiser-chez-binary "chezscheme9.5")))
-
-;;   ;; (setq geiser-active-implementations '(guile chez))
-;;   (setq geiser-default-implementation 'guile)
-;;   (setq geiser-active-implementations '(guile chez))
-
-;;   (setq geiser-mode-start-repl-p t)
-;;   (add-to-list 'auto-mode-alist '("\\.scheme\\'" . scheme-mode))
-;;   ;; (add-to-list 'auto-mode-alist '("\\.racket\\'" . scheme-mode))
-;;   ;; (add-to-list 'auto-mode-alist '("\\.rkt\\'" . scheme-mode))
-;;   (add-hook 'scheme-mode-hook 'geiser-mode)
-;;   :ensure t)
-
+  (after-load 'geiser-mode
+    (define-key geiser-mode-map (kbd "C-c C-c") 'geiser-eval-definition)))
 
 (provide 'init-scheme)
