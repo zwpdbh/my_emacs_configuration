28c5c157ffce959e98f9e6ae1c5a68d623429b11
zwpdbh <hyperion_z@outlook.com>
2020-06-10 00:48:06 +0800

    >

--- lisp/init-flycheck.el
+++ lisp/init-flycheck.el
@@ -2,24 +2,24 @@
 ;;; Commentary:
 ;;; Code:
 
-;; (when (maybe-require-package 'flycheck)
-;;   (add-hook 'after-init-hook 'global-flycheck-mode)
-;;   (setq flycheck-display-errors-function #'flycheck-display-error-messages-unless-error-list)
+(when (maybe-require-package 'flycheck)
+  ;; (add-hook 'after-init-hook 'global-flycheck-mode)
+  (setq flycheck-display-errors-function #'flycheck-display-error-messages-unless-error-list)
 
-;;   (when (maybe-require-package 'flycheck-color-mode-line)
-;;     (add-hook 'flycheck-mode-hook 'flycheck-color-mode-line-mode)))
+  (when (maybe-require-package 'flycheck-color-mode-line)
+    (add-hook 'flycheck-mode-hook 'flycheck-color-mode-line-mode)))
 
-(use-package flycheck
-  :diminish
-  :ensure t
-  :config
-  (progn
-    (use-package flycheck-color-mode-line
-      :ensure t
-      :after (flycheck)
-      :config
-      (add-hook 'flycheck-mode-hook 'flycheck-color-mode-line-mode))
-    (setq flycheck-display-errors-function #'flycheck-display-error-messages-unless-error-list)))
+;; (use-package flycheck
+;;   :diminish
+;;   :ensure t
+;;   :config
+;;   (progn
+;;     (use-package flycheck-color-mode-line
+;;       :ensure t
+;;       :after (flycheck)
+;;       :config
+;;       (add-hook 'flycheck-mode-hook 'flycheck-color-mode-line-mode))
+;;     (setq flycheck-display-errors-function #'flycheck-display-error-messages-unless-error-list)))
 
 (provide 'init-flycheck)
 ;;; init-flycheck.el ends here
