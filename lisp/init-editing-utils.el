a350a86b0b553a04a1ef4bab45f3009f59a8cc8b
zwpdbh <hyperion_z@outlook.com>
2020-06-14 15:08:57 +0800

    < add some format on init-editing-utils.el

--- lisp/init-editing-utils.el
+++ lisp/init-editing-utils.el
@@ -29,23 +29,22 @@ If the new path's directories does not exist, create them."
 ;;----------------------------------------------------------------------------
 ;; Some basic preferences
 ;;----------------------------------------------------------------------------
-(setq-default
- blink-cursor-interval 0.4
- bookmark-default-file (expand-file-name ".bookmarks.el" user-emacs-directory)
- buffers-menu-max-size 30
- case-fold-search t
- column-number-mode t
- delete-selection-mode t
- ediff-split-window-function 'split-window-horizontally
- ediff-window-setup-function 'ediff-setup-windows-plain
- indent-tabs-mode nil
- mouse-yank-at-point t
- save-interprogram-paste-before-kill t
- scroll-preserve-screen-position 'always
- set-mark-command-repeat-pop t
- tooltip-delay 1.5
- truncate-lines nil
- truncate-partial-width-windows nil)
+(setq-default blink-cursor-interval 0.4
+              bookmark-default-file (expand-file-name ".bookmarks.el" user-emacs-directory)
+              buffers-menu-max-size 30
+              case-fold-search t
+              column-number-mode t
+              delete-selection-mode t
+              ediff-split-window-function 'split-window-horizontally
+              ediff-window-setup-function 'ediff-setup-windows-plain
+              indent-tabs-mode nil
+              mouse-yank-at-point t
+              save-interprogram-paste-before-kill t
+              scroll-preserve-screen-position 'always
+              set-mark-command-repeat-pop t
+              tooltip-delay 1.5
+              truncate-lines nil
+              truncate-partial-width-windows nil)
 
 (add-hook 'after-init-hook 'global-auto-revert-mode)
 (setq global-auto-revert-non-file-buffers t
