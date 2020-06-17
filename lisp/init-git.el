16d8c0a4de806c55c130b4dd69b39c01e7944d01
zwpdbh <hyperion_z@outlook.com>
2020-06-17 08:51:30 +0800

    <<

--- lisp/init-git.el
+++ lisp/init-git.el
@@ -20,7 +20,6 @@
 
   ;; Hint: customize `magit-repository-directories' so that you can use C-u M-F12 to
   ;; quickly open magit on any one of your projects.
-  (global-set-key [(meta f12)] 'magit-status)
   (global-set-key (kbd "C-x g") 'magit-status)
   (global-set-key (kbd "C-x M-g") 'magit-dispatch)
 
