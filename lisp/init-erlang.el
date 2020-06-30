caad0eac6746cd0966d6d25928a403d868eb0872
zwpdbh <hyperion_z@outlook.com>
2020-06-30 23:36:50 +0800

    >>

--- lisp/init-erlang.el
+++ lisp/init-erlang.el
@@ -1,5 +1,3 @@
-;;; init-erlang.el --- Support with erlang -*- lexical-binding: t -*-
-
 ;; install erlang
 ;; https://medium.com/erlang-central/erlang-quick-install-9c5dcaa5b634
 ;; on windows: https://www.erlang.org/downloads
@@ -12,25 +10,29 @@
 
 
 (cond (*win64*
-       (add-to-list 'load-path "c:/Program Files/erl-23.0/lib/tools-3.4/emacs")
        (add-to-list 'exec-path "c:/Program Files/erl-23.0/bin")
+       (add-to-list 'load-path "c:/Program Files/erl-23.0/lib/tools-3.4/emacs")
        (setq erlang-root-dir "c:/Program Files/erl-23.0"))
       (t
-       (add-to-list 'load-path "/usr/lib/erlang/lib/tools-2.11.1/emacs")
        (add-to-list 'exec-path "/usr/bin")
+       (add-to-list 'load-path "/usr/lib/erlang/lib/tools-2.11.1/emacs")
        (add-to-list 'exec-path "/usr/local/bin")
        (setq erlang-root-dir "/usr/local/otp")))
 
-
 (when (executable-find "erl")
   (require 'erlang-start)
+
   ;; The OTP emacs mode doesn't come with some of my commonly used files that are Erlang code or terms.
   (add-to-list 'auto-mode-alist '("rebar.config" . erlang-mode))
   (add-to-list 'auto-mode-alist '("rebar.config.script" . erlang-mode))
   (add-to-list 'auto-mode-alist '("app.config" . erlang-mode))
-  (add-to-list 'auto-mode-alist '("\\.erlang$" . erlang-mode)))
-
-
+  (add-to-list 'auto-mode-alist '("\\.erlang$" . erlang-mode))
+  ;;TODO: ??Have to set it again to make erlang could be loaded
+  (add-hook 'after-init-hook '(lambda ()
+                                (cond (*win64*
+                                       (add-to-list 'load-path "c:/Program Files/erl-23.0/lib/tools-3.4/emacs"))
+                                      (t
+                                       (add-to-list 'load-path "/usr/lib/erlang/lib/tools-2.11.1/emacs"))))))
 
 
 (provide 'init-erlang)
