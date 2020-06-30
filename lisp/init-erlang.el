6dd2d85151d22324fc2352d0ede67a2dcd1b776f
zwpdbh <hyperion_z@outlook.com>
2020-06-30 11:27:47 +0800

    >>

---
+++ lisp/init-erlang.el
@@ -0,0 +1,24 @@
+;;; init-erlang.el --- Support with erlang -*- lexical-binding: t -*-
+
+;; refs:
+;; http://alexott.net/en/writings/emacs-devenv/EmacsErlang.html
+;; https://www.lambdacat.com/post-modern-emacs-setup-for-erlang/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+(provide 'init-erlang)
+;;; init-erlang ends here
