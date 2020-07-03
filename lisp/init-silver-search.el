cbba83b72a2f9adbc249531e2118cc29b031f0d0
Zhao, Wei <frank.zhao@siemens.com>
2020-07-03 12:51:13 +0800

    >>

--- lisp/init-silver-search.el
+++ lisp/init-silver-search.el
@@ -2,19 +2,9 @@
   :ensure t
   :defer t)
 
-;; (use-package helm-ag
-;;   :init
-;;   (custom-set-variables
-;;    '(helm-ag-base-command "ag --nocolor --nogroup --ignore-case")
-;;    '(helm-ag-command-option "--all-text")
-;;    '(helm-ag-insert-at-point 'symbol)
-;;    '(helm-ag-ignore-buffer-patterns '("\\.txt\\'" "\\.mkd\\'")))
-;;   :ensure t
-;;   :defer t
-;;   :commands (helm-projectile-ag))
-
+;; available options see: https://github.com/emacsorphanage/helm-ag
 (when (maybe-require-package 'helm-ag)
-  (custom-set-variables
-   '(helm-ag-command-option "--hidden --ignore .git")
-   '(helm-ag-insert-at-point 'symbol))
-  (provide 'init-silver-search))
+  (setq helm-ag-command-option "--hidden --ignore .git"
+        helm-ag-insert-at-point 'symbol))
+
+(provide 'init-silver-search)
