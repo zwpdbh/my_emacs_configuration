0ff017b2b5b435e5cafddd0dfd624a8d5001a27b
Zhao, Wei <frank.zhao@siemens.com>
2020-07-27 15:16:11 +0800

    >>

--- lisp/init-eglot.el
+++ lisp/init-eglot.el
@@ -1,19 +1,14 @@
 (when (maybe-require-package 'eglot)
+  (setq my-eglot-mode-set '(js-mode
+                            python-mode
+                            sh-mode
+                            c-mode-common
+                            c-mode
+                            c++-mode))
 
-  (defun zw/eglot-key-bindings-for (some-mode)
-    (let ((that-mode-map (concat some-mode "-map")))
-      (define-key that-mode-map (kbd "M-.") 'lsp-ui-peek-find-definitions)
-      (if (display-graphic-p)
-          (define-key that-mode-map (kbd "M-/") 'lsp-ui-peek-find-references)
-        (define-key that-mode-map (kbd "C-x .") 'lsp-ui-peek-find-references))))
-  
-  (dolist (each-hook (list
-                      'js-mode-hook
-                      'python-mode-hook
-                      'sh-mode-hook
-                      'c-mode-common-hook
-                      'c-mode-hook
-                      'c++-mode-hook))
-    (add-hook each-hook 'eglot-ensure)))
+  (dolist (each-mode my-eglot-mode-set)
+    (add-hook (intern (format "%s-hook" each-mode))
+              #'(lambda ()
+                  (eglot-ensure)))))
 
 (provide 'init-eglot)
