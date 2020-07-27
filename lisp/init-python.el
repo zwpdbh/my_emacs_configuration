f53ec36e3de23ee2ac3a25411e0564e17933e68b
Zhao, Wei <frank.zhao@siemens.com>
2020-07-27 17:07:21 +0800

    >>

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -9,8 +9,6 @@
         (expand-file-name "~/python-language-server/output/bin/Release/"))
   (setq lsp-python-ms-executable
         "~/python-language-server/output/bin/Release/Microsoft.Python.LanguageServer")
-  (add-hook 'python-mode-hook 'lsp-mode)
-  ;; (add-hook 'python-mode-hook #'smartparens-mode)
   :ensure t)
 
 
