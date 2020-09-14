1eec7da6f80550b551db9260bea61db8b9079848
zwpdbh <hyperion_z@outlook.com>
2020-09-14 23:07:24 +0800

    << disable nox and lsp too slow

--- lisp/init-lsp.el
+++ lisp/init-lsp.el
@@ -1,79 +1,79 @@
-(when (maybe-require-package 'lsp-mode)
-  (when (symbol-function 'helm)
-    (maybe-require-package 'helm-lsp))
-  (when (symbol-function 'treemacs)
-    (maybe-require-package 'lsp-treemacs))
-  (when (symbol-function 'company-mode)
-    (maybe-require-package 'company-lsp))
-  (maybe-require-package 'lsp-ui))
+;; (when (maybe-require-package 'lsp-mode)
+;;   (when (symbol-function 'helm)
+;;     (maybe-require-package 'helm-lsp))
+;;   (when (symbol-function 'treemacs)
+;;     (maybe-require-package 'lsp-treemacs))
+;;   (when (symbol-function 'company-mode)
+;;     (maybe-require-package 'company-lsp))
+;;   (maybe-require-package 'lsp-ui))
 
 
-(after-load 'company-lsp
-  (setq company-lsp-cache-candidates nil)
-  (setq company-lsp-async t)
-  (setq company-lsp-enable-recompletion t))
+;; (after-load 'company-lsp
+;;   (setq company-lsp-cache-candidates nil)
+;;   (setq company-lsp-async t)
+;;   (setq company-lsp-enable-recompletion t))
 
-(after-load 'lsp-mode
-  (setq lsp-message-project-root-warning t)
-  ;; change nil to 't to enable logging of packets between emacs and the LS
-  ;; this was invaluable for debugging communication with the MS Python Language Server
-  ;; and comparing this with what vs.code is doing
-  (setq lsp-print-io nil)
-  (lsp-ui-mode t))
+;; (after-load 'lsp-mode
+;;   (setq lsp-message-project-root-warning t)
+;;   ;; change nil to 't to enable logging of packets between emacs and the LS
+;;   ;; this was invaluable for debugging communication with the MS Python Language Server
+;;   ;; and comparing this with what vs.code is doing
+;;   (setq lsp-print-io nil)
+;;   (lsp-ui-mode t))
 
-(after-load 'lsp-ui
-  ;; Establishing keybindings for lsp-ui-mode
-  ;; Variable lsp-ui-mode-map is available only after lsp-ui.el or lsp-ui.elc is loaded.
-  (setq lsp-ui-peek-force-fontify t)
-  (setq lsp-ui-imenu-enable t)
-  (setq lsp-ui-sideline-ignore-duplicate t)
-  (setq lsp-ui-sideline-enable nil)
-  (setq lsp-ui-doc-enable nil)
-  (setq lsp-ui-peek-fontify 'always)
+;; (after-load 'lsp-ui
+;;   ;; Establishing keybindings for lsp-ui-mode
+;;   ;; Variable lsp-ui-mode-map is available only after lsp-ui.el or lsp-ui.elc is loaded.
+;;   (setq lsp-ui-peek-force-fontify t)
+;;   (setq lsp-ui-imenu-enable t)
+;;   (setq lsp-ui-sideline-ignore-duplicate t)
+;;   (setq lsp-ui-sideline-enable nil)
+;;   (setq lsp-ui-doc-enable nil)
+;;   (setq lsp-ui-peek-fontify 'always)
 
-  (define-key lsp-ui-mode-map [remap xref-find-definitions] #'lsp-ui-peek-find-definitions)
-  (define-key lsp-ui-mode-map [remap xref-find-references] #'lsp-ui-peek-find-references)
+;;   (define-key lsp-ui-mode-map [remap xref-find-definitions] #'lsp-ui-peek-find-definitions)
+;;   (define-key lsp-ui-mode-map [remap xref-find-references] #'lsp-ui-peek-find-references)
 
-  ;; customize lsp-ui-peek appearance
-  (add-hook 'lsp-ui-mode-hook
-            '(lambda ()
-               (set-face-attribute 'lsp-ui-peek-selection nil
-                                   :foreground (face-foreground 'hl-line t t)
-                                   :background (face-background 'hl-line nil t)
-                                   :underline nil
-                                   :weight 'bold)
-               
-               (set-face-attribute 'lsp-ui-peek-highlight nil
-                                   :foreground "Yellow"
-                                   :background (face-background 'default t t)
-                                   :underline nil
-                                   :box nil
-                                   :weight 'normal)
-               (set-face-attribute 'lsp-ui-peek-peek nil
-                                   :foreground (face-foreground 'default t t)
-                                   :background (face-background 'default t t)))))
+;;   ;; customize lsp-ui-peek appearance
+;;   (add-hook 'lsp-ui-mode-hook
+;;             '(lambda ()
+;;                (set-face-attribute 'lsp-ui-peek-selection nil
+;;                                    :foreground (face-foreground 'hl-line t t)
+;;                                    :background (face-background 'hl-line nil t)
+;;                                    :underline nil
+;;                                    :weight 'bold)
 
-
-(add-hook 'lsp-mode-hook
-          '(lambda ()
-             ;; Code run from x-mode-hook is for buffer-specific things which means
-             ;; run the code for every x-mode buffer
-             (setq-local company-backends (add-to-list 'company-backends
-                                                       'company-lsp))))
-
-(defun zw/customize-lsp-ui-key-bindings ()
-  (interactive)
-  (when (featurep 'lsp-ui)
-    (define-key (current-local-map) (kbd "M-.") 'lsp-ui-peek-find-definitions)
-    (define-key (current-local-map) (kbd "M-/") 'lsp-ui-peek-find-references)))
+;;                (set-face-attribute 'lsp-ui-peek-highlight nil
+;;                                    :foreground "Yellow"
+;;                                    :background (face-background 'default t t)
+;;                                    :underline nil
+;;                                    :box nil
+;;                                    :weight 'normal)
+;;                (set-face-attribute 'lsp-ui-peek-peek nil
+;;                                    :foreground (face-foreground 'default t t)
+;;                                    :background (face-background 'default t t)))))
 
 
-(dolist (each-mode zw/lsp-clients-set)
-  (let ((each-mode-hook (intern (format "%s-hook" each-mode))))
-    (add-hook each-mode-hook
-              #'(lambda ()
-                  (lsp)
-                  (zw/customize-lsp-ui-key-bindings)))))
+;; (add-hook 'lsp-mode-hook
+;;           '(lambda ()
+;;              ;; Code run from x-mode-hook is for buffer-specific things which means
+;;              ;; run the code for every x-mode buffer
+;;              (setq-local company-backends (add-to-list 'company-backends
+;;                                                        'company-lsp))))
+
+;; (defun zw/customize-lsp-ui-key-bindings ()
+;;   (interactive)
+;;   (when (featurep 'lsp-ui)
+;;     (define-key (current-local-map) (kbd "M-.") 'lsp-ui-peek-find-definitions)
+;;     (define-key (current-local-map) (kbd "M-/") 'lsp-ui-peek-find-references)))
 
 
-(provide 'init-lsp)
+;; (dolist (each-mode zw/lsp-clients-set)
+;;   (let ((each-mode-hook (intern (format "%s-hook" each-mode))))
+;;     (add-hook each-mode-hook
+;;               #'(lambda ()
+;;                   (lsp)
+;;                   (zw/customize-lsp-ui-key-bindings)))))
+
+
+;; (provide 'init-lsp)
--- lisp/init-nox.el
+++ lisp/init-nox.el
@@ -1,22 +1,22 @@
-(add-to-list 'load-path
-             "~/.emacs.d/site-lisp/posfram")
+;; (add-to-list 'load-path
+;;              "~/.emacs.d/site-lisp/posfram")
 
-(unless sys/win32p
-  (require 'posframe))
+;; (unless sys/win32p
+;;   (require 'posframe))
 
 
-(add-to-list 'load-path
-             "~/.emacs.d/site-lisp/nox")
-(require 'nox)
+;; (add-to-list 'load-path
+;;              "~/.emacs.d/site-lisp/nox")
+;; (require 'nox)
 
-(dolist (each-mode zw/lsp-clients-set)
-  (add-hook (intern (format "%s-hook" each-mode))
-            #'(lambda ()
-                (nox-ensure))))
+;; (dolist (each-mode zw/lsp-clients-set)
+;;   (add-hook (intern (format "%s-hook" each-mode))
+;;             #'(lambda ()
+;;                 (nox-ensure))))
 
-(add-hook 'nox-managed-mode-hook
-          '(lambda ()
-             (when (nox-managed-p)
-               (zw/set-company-backends-global))))
+;; (add-hook 'nox-managed-mode-hook
+;;           '(lambda ()
+;;              (when (nox-managed-p)
+;;                (zw/set-company-backends-global))))
 
-(provide 'init-nox)
+;; (provide 'init-nox)
