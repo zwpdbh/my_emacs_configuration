3b2508b05644586af3ec50920f319b238c4a1d57
Zhao, Wei <frank.zhao@siemens.com>
2020-09-16 10:59:45 +0800

    << fix issue: sometimes the key remap doesn't work, so use kbd.

--- lisp/init-keybinding.el
+++ lisp/init-keybinding.el
@@ -72,11 +72,18 @@
 
 ;; when runing in -nw, use xclip
 (unless (display-graphic-p)
-  (global-set-key [remap whole-line-or-region-kill-ring-save] 'copy-to-x-clipboard)
-  (global-set-key [remap whole-line-or-region-kill-region] 'cut-to-x-clipboard)
-  (global-set-key [remap whole-line-or-region-yank] 'paste-from-x-clipboard)
+  ;; (global-set-key [remap whole-line-or-region-kill-ring-save] 'copy-to-x-clipboard)
+  (global-unset-key (kbd "M-w"))
+  (global-set-key (kbd "M-w") 'copy-to-x-clipboard)  
+
+  ;; (global-set-key [remap whole-line-or-region-kill-region] 'cut-to-x-clipboard)
+  (global-unset-key (kbd "C-w"))
+  (global-set-key (kbd "C-w") 'cut-to-x-clipboard)  
+
+  ;; (global-set-key [remap whole-line-or-region-yank] 'paste-from-x-clipboard)
   (when (fboundp 'paste-from-x-clipboard)
-    (global-set-key [remap yank] 'paste-from-x-clipboard)))
+    (global-unset-key (kbd "C-y"))
+    (global-set-key (kbd "C-y") 'paste-from-x-clipboard)))
 
 ;; for ivy minibuffer
 (when (fboundp 'ivy-previous-line-and-call)
