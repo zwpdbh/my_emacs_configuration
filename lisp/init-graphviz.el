7cba4e5691331b0256c8f7f0f1b86f6ae07c53c1
Zhao, Wei <frank.zhao@siemens.com>
2020-11-07 15:26:11 +0800

    << init-org-babel for graphviz

--- lisp/init-graphviz.el
+++ lisp/init-graphviz.el
@@ -7,7 +7,7 @@
                (require 'ob-dot)
                (add-to-list 'zw/org-babel-evaluate-whitelist "dot")
                (add-to-list 'zw/org-babel-load-language-list '(dot . t))
-               (add-to-list 'org-structure-template-alist '("dot" . "src dot :cmdline -Kdot -Tpng :file img/tmp.png"))
+               (add-to-list 'org-structure-template-alist '("dot" . "src dot :cmdline -Kdot -Tpng :file tmp.png"))
                
                ;; set the major-mode for edit babel dot src block 
                (add-to-list 'org-src-lang-modes (quote ("dot" . graphviz-dot))))))
