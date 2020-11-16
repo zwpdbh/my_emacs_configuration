1dc9d2dc98bdd56f96aabf103e231925b79c46ff
Zhao, Wei <frank.zhao@siemens.com>
2020-11-16 17:26:09 +0800

    <<<

--- lisp/init-helm.el
+++ lisp/init-helm.el
@@ -78,17 +78,17 @@
           helm-imenu-fuzzy-match t
           helm-semantic-fuzzy-match t)
 
-    ;; (defun spacemacs//helm-hide-minibuffer-maybe ()
-    ;;   "Hide minibuffer in Helm session if we use the header line as input field."
-    ;;   (when (with-helm-buffer helm-echo-input-in-header-line)
-    ;;     (let ((ov (make-overlay (point-min) (point-max) nil nil t)))
-    ;;       (overlay-put ov 'window (selected-window))
-    ;;       (overlay-put ov 'face
-    ;;                    (let ((bg-color (face-background 'default nil)))
-    ;;                      `(:background ,bg-color :foreground ,bg-color)))
-    ;;       (setq-local cursor-type nil))))
-    ;; (add-hook 'helm-minibuffer-set-up-hook
-    ;;           'spacemacs//helm-hide-minibuffer-maybe)
+    (defun spacemacs//helm-hide-minibuffer-maybe ()
+      "Hide minibuffer in Helm session if we use the header line as input field."
+      (when (with-helm-buffer helm-echo-input-in-header-line)
+        (let ((ov (make-overlay (point-min) (point-max) nil nil t)))
+          (overlay-put ov 'window (selected-window))
+          (overlay-put ov 'face
+                       (let ((bg-color (face-background 'default nil)))
+                         `(:background ,bg-color :foreground ,bg-color)))
+          (setq-local cursor-type nil))))
+    (add-hook 'helm-minibuffer-set-up-hook
+              'spacemacs//helm-hide-minibuffer-maybe)
 
     (setq helm-autoresize-max-height 45)
     (setq helm-autoresize-min-height 30)    
