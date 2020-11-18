c1771230e027efb0ab2a5dbccaee7df67631dc4c
zwpdbh <hyperion_z@outlook.com>
2020-11-18 10:09:25 +0800

    << add ob-babel for css

--- lisp/init-org-ob-browser.el
+++ lisp/init-org-ob-browser.el
@@ -2,10 +2,16 @@
 (when (maybe-require-package 'ob-browser)
   (add-hook 'org-mode-hook
             '(lambda ()
-               ;; since yaml mode is not supported by org, create the command yourself
-               ;; (defun org-babel-execute:browser (body params) body)
                (add-to-list 'zw/org-babel-evaluate-whitelist "browser")
                (add-to-list 'zw/org-babel-load-language-list '(browser . t))
-               (add-to-list 'org-structure-template-alist '("browser" . "src browser :out demo.png")))))
+               (add-to-list 'org-structure-template-alist '("browser" . "src browser :out demo.png"))))
+
+  (add-hook 'org-mode-hook
+            '(lambda ()
+               ;; since yaml mode is not supported by org, create the command yourself
+               (defun org-babel-execute:css (body params) body)
+               
+               (add-to-list 'zw/org-babel-load-language-list '(css . t))
+               (add-to-list 'org-structure-template-alist '("css" . "src css")))))
 
 (provide 'init-org-ob-browser)
