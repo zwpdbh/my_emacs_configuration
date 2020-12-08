57dc978f36ef8df5fc68fd322bd199f57cad57cf
zwpdbh <hyperion_z@outlook.com>
2020-12-08 22:31:42 +0800

    <

--- lisp/init-themes-customization-for-weyland.el
+++ lisp/init-themes-customization-for-weyland.el
@@ -8,7 +8,7 @@
   (setq helm-buffer-max-length 36)
 
   (set-face-attribute 'helm-ff-directory nil
-                      :foreground "#20b2aa"
+                      :foreground "#67bfba"
                       :background (face-background 'default t t)
                       :weight 'bold)
   (set-face-attribute 'helm-ff-dotted-directory nil
@@ -31,6 +31,13 @@
   (set-face-attribute 'header-line nil
                       :height 1.0
                       :underline nil)
+
+  ;; set highlight face
+  (set-face-attribute 'highlight nil
+                      :weight 'bold
+                      :foreground "#86dc2f"
+                      :background (face-background 'default t t))
+  
   ;; disable fringe 
   (set-fringe-mode 0))
 
