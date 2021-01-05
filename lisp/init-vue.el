a5c9624846fda281bd70bf7d930cfd8cb3e0d8a5
Zhao, Wei <frank.zhao@siemens.com>
2021-01-05 15:40:32 +0800

    << inhibit electric-indent in .vue file

--- lisp/init-vue.el
+++ lisp/init-vue.el
@@ -2,6 +2,8 @@
 (add-to-list 'auto-mode-alist '("\\.vue\\'" . web-mode))
 (add-hook 'web-mode-hook
           '(lambda ()
+             ;; reindentation is not appropriate for dealing with .vue file.
+             (setq-local electric-indent-inhibit t)
              (zw/counsel-etags-setup)))
 
 (provide 'init-vue)
