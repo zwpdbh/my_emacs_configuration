e66d73daa0a8982d3b26150fe3e1fed951143456
Zhao, Wei <frank.zhao@siemens.com>
2021-01-12 15:23:04 +0800

    < put consult-selectrum outside of consult package configuration

--- lisp/init-consult.el
+++ lisp/init-consult.el
@@ -1,16 +1,12 @@
 (when (maybe-require-package 'consult)
   ;; (setq consult-narrow-key "l")
-  
   (global-set-key (kbd "C-x M-:") 'consult-complex-command)
   (global-set-key (kbd "C-x b") 'consult-buffer)
   (global-set-key (kbd "C-s") 'consult-line)
-  (global-set-key (kbd "C-r") 'consult-outline)
-  
-  ;; (after-load 'consult        
-  ;;   (consult-preview-mode))
-  
-  (when (maybe-require-package 'consult-selectrum)
-    (after-load 'selectrum
-      (require 'consult-selectrum))))
+  (global-set-key (kbd "C-r") 'consult-outline))
+
+(when (maybe-require-package 'consult-selectrum)
+  (after-load 'selectrum
+    (require 'consult-selectrum)))
 
 (provide 'init-consult)
