e89fad77da9d470a245af50afc637e390f276166
zwpdbh <hyperion_z@outlook.com>
2021-01-29 20:04:14 +0800

    << speed up org-hugo

--- lisp/init-org-blog.el
+++ lisp/init-org-blog.el
@@ -9,78 +9,79 @@
 
 
 (when (maybe-require-package 'ox-hugo)
-  (defun org-hugo-new-subtree-post-capture-template ()
-    "Returns `org-capture' template string for new Hugo post.
+  (after-load 'org
+    (defun org-hugo-new-subtree-post-capture-template ()
+      "Returns `org-capture' template string for new Hugo post.
 See `org-capture-templates' for more information."
-    (let* (;; http://www.holgerschurig.de/en/emacs-blog-from-org-to-hugo/
-           (date (format-time-string (org-time-stamp-format  :inactive) (org-current-time)))
-           (title (read-from-minibuffer "Post Title: ")) ;Prompt to enter the post title
-           (fname (org-hugo-slug title)))
-      (mapconcat #'identity
-                 `(
-                   ,(concat "* TODO " title)
-                   "  :PROPERTIES:"
-                   ,(concat "  :EXPORT_FILE_NAME: " fname)
-                   ,(concat "  :EXPORT_DATE: " date) ;Enter current date and time
-                   ,(concat "  :EXPORT_OPTIONS: " ":toc:t") ; Table of Contents, see: https://ox-hugo.scripter.co/doc/org-toc/
-                   ,(concat "  :EXPORT_HUGO_CUSTOM_FRONT_MATTER+: "  ":weight 10 :autoCollapseToc true :mathjax true :contentCopyright MIT :author \"Z wei\"")
-                   "  :END:"
-                   "  %?\n")
-                 "\n")))
+      (let* (;; http://www.holgerschurig.de/en/emacs-blog-from-org-to-hugo/
+             (date (format-time-string (org-time-stamp-format  :inactive) (org-current-time)))
+             (title (read-from-minibuffer "Post Title: ")) ;Prompt to enter the post title
+             (fname (org-hugo-slug title)))
+        (mapconcat #'identity
+                   `(
+                     ,(concat "* TODO " title)
+                     "  :PROPERTIES:"
+                     ,(concat "  :EXPORT_FILE_NAME: " fname)
+                     ,(concat "  :EXPORT_DATE: " date) ;Enter current date and time
+                     ,(concat "  :EXPORT_OPTIONS: " ":toc:t") ; Table of Contents, see: https://ox-hugo.scripter.co/doc/org-toc/
+                     ,(concat "  :EXPORT_HUGO_CUSTOM_FRONT_MATTER+: "  ":weight 10 :autoCollapseToc true :mathjax true :contentCopyright MIT :author \"Z wei\"")
+                     "  :END:"
+                     "  %?\n")
+                   "\n")))
 
-  (defvar hugo-org-path "~/code/capture-org/"
-    "define the place where we put our org files for hugo")
-  (defvar org-capture-todo (concat hugo-org-path "todo.org"))
-  (defvar org-capture-computer-science (concat hugo-org-path "computer-science/" "computer-science.org"))
-  (defvar org-capture-emacs (concat hugo-org-path "emacs/" "emacs.org"))
-  (defvar org-capture-math (concat hugo-org-path "mathematics/" "mathematics.org"))
-  (defvar org-capture-software (concat hugo-org-path "software-engineering/" "software-engineering.org"))
-  (defvar org-capture-tools (concat hugo-org-path "tools/" "tools.org"))
+    (defvar hugo-org-path "~/code/capture-org/"
+      "define the place where we put our org files for hugo")
+    (defvar org-capture-todo (concat hugo-org-path "todo.org"))
+    (defvar org-capture-computer-science (concat hugo-org-path "computer-science/" "computer-science.org"))
+    (defvar org-capture-emacs (concat hugo-org-path "emacs/" "emacs.org"))
+    (defvar org-capture-math (concat hugo-org-path "mathematics/" "mathematics.org"))
+    (defvar org-capture-software (concat hugo-org-path "software-engineering/" "software-engineering.org"))
+    (defvar org-capture-tools (concat hugo-org-path "tools/" "tools.org"))
 
-  (setq org-export-with-author nil)
-  (setq org-hugo-export-with-toc 2)
-  (setq org-capture-templates
-        '(("h" "Hugo post")
-          
-          ("t" "todo"
-           entry (file org-capture-todo)
-           "* TODO %? :TODO: \n Added:%T\n"
-           :clock-in t :clock-resume t)
+    (setq org-export-with-author nil)
+    (setq org-hugo-export-with-toc 2)
+    (setq org-capture-templates
+          '(("h" "Hugo post")
+            
+            ("t" "todo"
+             entry (file org-capture-todo)
+             "* TODO %? :TODO: \n Added:%T\n"
+             :clock-in t :clock-resume t)
 
-          ;; ;; This will make the captured note appear as a second level heading under
-          ;; ;; the first level heading Computer-Science
-          ;; ("hc" "Computer-Science"
-          ;;  entry (file+olp org-capture-computer-science "Computer-Science")
-          ;;  (function org-hugo-new-subtree-post-capture-template)
-          ;;  :clock-in t :clock-resume t)
+            ;; ;; This will make the captured note appear as a second level heading under
+            ;; ;; the first level heading Computer-Science
+            ;; ("hc" "Computer-Science"
+            ;;  entry (file+olp org-capture-computer-science "Computer-Science")
+            ;;  (function org-hugo-new-subtree-post-capture-template)
+            ;;  :clock-in t :clock-resume t)
 
-          ;; This will make the captured note appear as the first level heading in the computer-science.org
-          ("hc" "Computer-Science"
-           entry (file org-capture-computer-science)
-           (function org-hugo-new-subtree-post-capture-template)
-           :clock-in t :clock-resume t)
+            ;; This will make the captured note appear as the first level heading in the computer-science.org
+            ("hc" "Computer-Science"
+             entry (file org-capture-computer-science)
+             (function org-hugo-new-subtree-post-capture-template)
+             :clock-in t :clock-resume t)
 
-          ("he" "Emacs"
-           entry (file org-capture-emacs)
-           (function org-hugo-new-subtree-post-capture-template)
-           :clock-in t :clock-resume t)
+            ("he" "Emacs"
+             entry (file org-capture-emacs)
+             (function org-hugo-new-subtree-post-capture-template)
+             :clock-in t :clock-resume t)
 
-          ("hm" "Mathematics"
-           entry (file org-capture-math)
-           (function org-hugo-new-subtree-post-capture-template)
-           :clock-in t :clock-resume t)
+            ("hm" "Mathematics"
+             entry (file org-capture-math)
+             (function org-hugo-new-subtree-post-capture-template)
+             :clock-in t :clock-resume t)
 
-          ("hs" "Software-Engineering"
-           entry (file org-capture-software)
-           (function org-hugo-new-subtree-post-capture-template)
-           :clock-in t :clock-resume t)
+            ("hs" "Software-Engineering"
+             entry (file org-capture-software)
+             (function org-hugo-new-subtree-post-capture-template)
+             :clock-in t :clock-resume t)
 
-          ("ht" "Tools"
-           entry (file org-capture-tools)
-           ;; entry (file org-capture-tools)
-           (function org-hugo-new-subtree-post-capture-template)
-           :clock-in t :clock-resume t)))
-  (require 'ox-hugo))
+            ("ht" "Tools"
+             entry (file org-capture-tools)
+             ;; entry (file org-capture-tools)
+             (function org-hugo-new-subtree-post-capture-template)
+             :clock-in t :clock-resume t)))
+    (require 'ox-hugo)))
 
 
 (provide 'init-org-blog)
