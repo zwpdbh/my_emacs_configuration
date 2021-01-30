da62c9116e32c481bf5ea803dd499364149cd1bf
zwpdbh <hyperion_z@outlook.com>
2021-01-30 15:39:23 +0800

    << sorted common-lisp-style

--- lisp/init-common-lisp-with-slime.el
+++ lisp/init-common-lisp-with-slime.el
@@ -1,3 +1,6 @@
+(add-to-list 'auto-mode-alist '("\\.lisp\\'" . lisp-mode))
+(add-to-list 'auto-mode-alist '("\\.cl\\'" . lisp-mode))
+
 (when (maybe-require-package 'slime)  
   ;; (require 'slime-autoloads)
   ;; set some contribs
@@ -43,35 +46,6 @@
           '(lambda ()
              (setq lisp-indent-function 'common-lisp-indent-function)))
 
-(after-load 'slime
-  ;; ;; Set the alignment of the indents under him
-  ;; ;; Learn more about customization of this case you can read
-  ;; ;; SLIME source, namely
-  ;; ;; %путь_к_slime%/contrib/slime-cl-indent.el
-  ;; (define-common-lisp-style "zw/common-lisp-indent-style"
-  ;;   "My custom indent style."
-  ;;   (:inherit "modern")
-  ;;   (:variables
-  ;;    (lisp-loop-indent-subclauses t)) 
-  ;;   (:indentation
-  ;;    (if (4 2 2))
-  ;;    (define (&lambda 2))
-  ;;    (with-gensyms ((&whole 4 &rest 1) &body))
-  ;;    (once-only (as with-gensyms))))
-  ;; (setq common-lisp-style-default "zw/common-lisp-indent-style")
-  (setq common-lisp-style-default "sbcl")
-  (setq common-lisp-style "sbcl")
-  
-  (define-key slime-mode-map  (kbd "C-c C-c") nil)
-  (define-key slime-mode-map  (kbd "C-c C-c") #'slime-eval-last-expression)
-  (define-key slime-mode-map  (kbd "C-c C-e") nil)
-  (define-key slime-mode-map  (kbd "C-c C-e") #'slime-eval-last-expression-in-repl)
-  (define-key slime-mode-map (kbd "C-c C-h") #'slime-documentation-lookup))
-
-
-(add-to-list 'auto-mode-alist '("\\.lisp\\'" . lisp-mode))
-(add-to-list 'auto-mode-alist '("\\.cl\\'" . lisp-mode))
-
 ;; set lisp system
 (if *win64*
     (setq my-ccl (executable-find "wx86cl64"))
@@ -94,6 +68,35 @@
  (my-clisp
   (setq inferior-lisp-program my-clisp)
   (setq slime-lisp-implementations `((clisp (,my-clisp))))))
+
+(after-load 'slime
+  ;; ;; Set the alignment of the indents under him
+  ;; ;; Learn more about customization of this case you can read
+  ;; ;; SLIME source, namely
+  ;; ;; %путь_к_slime%/contrib/slime-cl-indent.el
+  ;; (define-common-lisp-style "zw/common-lisp-indent-style"
+  ;;   "My custom indent style."
+  ;;   (:inherit "modern")
+  ;;   (:variables
+  ;;    (lisp-loop-indent-subclauses t)) 
+  ;;   (:indentation
+  ;;    (if (4 2 2))
+  ;;    (define (&lambda 2))
+  ;;    (with-gensyms ((&whole 4 &rest 1) &body))
+  ;;    (once-only (as with-gensyms))))
+  ;; (setq common-lisp-style-default "zw/common-lisp-indent-style")
+  (setq common-lisp-style-default "modern")
+  (if (search "sbcl" inferior-lisp-program)
+      (setq common-lisp-style "sbcl")
+    (setq common-lisp-style "modern"))
+
+  
+  (define-key slime-mode-map  (kbd "C-c C-c") nil)
+  (define-key slime-mode-map  (kbd "C-c C-c") #'slime-eval-last-expression)
+  (define-key slime-mode-map  (kbd "C-c C-e") nil)
+  (define-key slime-mode-map  (kbd "C-c C-e") #'slime-eval-last-expression-in-repl)
+  (define-key slime-mode-map (kbd "C-c C-h") #'slime-documentation-lookup))
+
 
 
 (provide 'init-common-lisp-with-slime)
