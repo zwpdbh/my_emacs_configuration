abb365d6a59bd421e590d03f0a2a90ca0c202617
zwpdbh <hyperion_z@outlook.com>
2021-02-09 17:09:28 +0800

    >>

--- lisp/init-themes-customization.el
+++ lisp/init-themes-customization.el
@@ -1,11 +1,234 @@
-(defun zw/customize-theme (pkg fn)
+(defun zw/customize-pkg-with-fn (pkg fn)
   (if (featurep pkg)
-      (apply #'fn)
-    (after-load pkg
-      (apply #'fn))))
-(defun zw/customize-themes-for-company ()
-  )
+      (funcall fn)
+    (after-load 'pkg
+      (funcall fn))))
 
+(defun zw/customize-themes-for-company ()
+  (set-face-attribute 'company-tooltip-common-selection nil
+                      :inherit nil
+                      :foreground "#a9b7ca"
+                      :weight 'bold)
+
+  (set-face-attribute 'company-tooltip-selection nil
+                      :inherit nil
+                      :foreground "#a9b7ca"
+                      :background "#3b3559")
+  
+  (set-face-attribute 'company-tooltip-annotation-selection nil
+                      :inherit nil
+                      :foreground "#59b9b4"))
+
+
+(defun zw/customize-themes-for-helm ()
+  ;; make helm related selection use underline
+  (set-face-attribute 'helm-selection nil
+                      :underline nil
+                      :foreground (face-foreground 'default t t)
+                      :weight 'bold
+                      :extend t)
+  (setq helm-buffer-max-length 36)
+
+  (set-face-attribute 'helm-ff-directory nil
+                      :foreground "#67bfba"
+                      :background (face-background 'default t t)
+                      :weight 'bold)
+  (set-face-attribute 'helm-ff-dotted-directory nil
+                      :foreground "#20b2aa"
+                      :background (face-background 'default t t)
+                      :weight 'bold))
+
+(defun zw/customize-themes-for-smartparens ()
+  (set-face-attribute 'sp-pair-overlay-face nil
+                      :foreground (face-foreground 'default t t)
+                      :background (face-background 'default t t)
+                      :inherit nil)
+  (set-face-attribute 'show-paren-mismatch nil      
+                      :foreground "WhiteSmoke"
+                      :background (face-background 'default t t)))
+
+(defun zw/customize-themes-for-selectrum ()
+  (set-face-attribute 'selectrum-current-candidate nil
+                      :inherit nil
+                      :underline nil
+                      :weight 'normal
+                      :foreground "Yellow"
+                      :background "#3b3559"))
+
+(defun zw/customize-themes-for-flycheck ()
+  (set-face-attribute 'flycheck-error nil
+                      :underline '(:color "firebrick" :style wave))
+  (set-face-attribute 'flycheck-warning nil
+                      :underline '(:color "yellow" :style wave)))
+
+(defun zw/customize-themes-for-consult ()
+  (set-face-attribute 'consult-preview-line nil
+                      :inherit nil
+                      :underline nil
+                      :weight 'normal
+                      :foreground "Yellow"
+                      :background "#3b3559")
+  
+  (set-face-attribute 'consult-file nil
+                      :inherit nil
+                      :foreground "#a9b7ca"))
+
+(defun zw/customize-themes-for-marginalia ()
+  (set-face-attribute 'marginalia-documentation nil
+                      :underline nil))
+
+(defun zw/customize-themes-for-orderless ()
+  (set-face-attribute 'orderless-match-face-0 nil
+                      :weight 'normal)
+  (set-face-attribute 'orderless-match-face-1 nil
+                      :weight 'normal)
+  (set-face-attribute 'orderless-match-face-2 nil
+                      :weight 'normal)
+  (set-face-attribute 'orderless-match-face-3 nil
+                      :weight 'normal))
+
+(defun zw/customize-themes-for-general ()
+  (when window-system
+    (set-cursor-color "tomato")
+    (setq-default cursor-type '(bar . 3)))
+
+  ;; make weyland theme use normal font size for header-line
+  (set-face-attribute 'header-line nil
+                      :height 1.0
+                      :underline nil)
+
+  ;; set highlight face
+  (set-face-attribute 'highlight nil
+                      :weight 'bold
+                      :foreground "#86dc2f"
+                      :background (face-background 'default t t))
+  ;; disable fringe 
+  (set-fringe-mode 0))
+
+(defun zw/customize-themes-for-web-mode ()
+  (set-face-attribute 'web-mode-current-element-highlight-face nil
+                      :weight 'normal
+                      :foreground "#f5f5f5"
+                      :background "#3b3559"))
+
+(defun zw/customize-themes-for-ivy ()
+  ;; make selection highlight-background expand full width of the minibuffer
+  (setcdr (assoc t ivy-format-functions-alist) #'ivy-format-function-line)
+
+  ;; make ivy current match background lighter
+  (set-face-attribute 'ivy-current-match nil
+                      :weight 'normal
+                      :foreground (face-foreground 'default t t)
+                      :background "#3b3559")
+  
+  ;; Face for ‘ivy’ minibuffer matches covered by inputs
+  (set-face-attribute 'ivy-minibuffer-match-face-1 nil
+                      :weight 'normal
+                      :underline nil
+                      :foreground "Yellow"
+                      :background (face-background 'default t t))
+  
+  ;; Face for ‘ivy’ minibuffer matches inputs 
+  (set-face-attribute 'ivy-minibuffer-match-face-2 nil
+                      :weight 'normal
+                      :underline nil
+                      :foreground "Yellow"
+                      :background (face-background 'default t t))
+  
+  ;; Face for "ivy" minibuffer matches other segement input
+  (set-face-attribute 'ivy-minibuffer-match-face-3 nil
+                      :weight 'normal
+                      :underline nil
+                      :foreground "Yellow"
+                      :background (face-background 'default t t))
+  
+  (set-face-attribute 'ivy-grep-info nil
+                      :weight 'normal
+                      :foreground "#4F9FD2"))
+
+
+(defun zw/customize-themes-for-swiper ()
+  (set-face-attribute 'swiper-line-face nil
+                      :inherit nil
+                      :weight 'bold
+                      :underline nil
+                      :foreground (face-foreground 'default t t)
+                      :background "#3b3559")
+  
+  (set-face-attribute 'swiper-match-face-1 nil
+                      :inherit nil
+                      :weight 'normal
+                      :underline t
+                      :foreground (face-foreground 'default t t)
+                      :background nil)
+
+  (set-face-attribute 'swiper-match-face-2 nil
+                      :inherit nil
+                      :weight 'normal
+                      :underline t
+                      :foreground (face-foreground 'default t t)
+                      :background nil)
+
+  (set-face-attribute 'swiper-match-face-3 nil
+                      :inherit nil
+                      :weight 'normal
+                      :underline t
+                      :foreground (face-foreground 'default t t)
+                      :background nil)
+
+  (set-face-attribute 'swiper-match-face-4 nil
+                      :inherit nil
+                      :weight 'normal
+                      :underline t
+                      :foreground (face-foreground 'default t t)
+                      :background nil))
+
+(defun zw/customize-themes-for-org ()
+  (setq org-emphasis-alist
+        '(("*" (:foreground "gold"))
+          ("/" italic)
+          ("_" underline)
+          ("=" (:background "maroon" :foreground "white"))
+          ("~" (:foreground "#fb8a69"))
+          ("+" (:strike-through t))))
+  
+  (set-face-attribute 'org-headline-done nil
+                      :weight 'bold
+                      :foreground "#606873"
+                      :strike-through nil))
+
+(defun zw/customize-themes-for-symbol-overlay ()
+  (set-face-attribute 'symbol-overlay-default-face nil
+                      :weight 'normal
+                      :foreground (face-foreground 'default t t)
+                      :background nil
+                      :underline t
+                      :inherit nil))
+
+(defun zw/customize-themes-for-js2 ()
+  (set-face-attribute 'js2-error nil
+                      :foreground "tomato"))
+
+(defun zw/customize-themes-for-indent-guide ()
+  (set-face-attribute 'indent-guide-face nil
+                      :foreground "DimGray"))
+
+(defun zw/customize-themes-for-eshell ()
+  (set-face-attribute 'eshell-prompt nil
+                      :weight 'normal
+                      :foreground "#86dc2f"))
+
+(defun zw/customize-themes-for-magit ()
+  (set-face-attribute 'diff-refine-removed nil
+                      :inherit nil
+                      :weight 'bold
+                      :foreground (face-foreground 'default t t)
+                      :background (face-background 'default t t))
+  (set-face-attribute 'diff-refine-added nil
+                      :inherit nil
+                      :weight 'bold
+                      :foreground (face-foreground 'default t t)
+                      :background (face-background 'default t t)))
 
 
 (provide 'init-themes-customization)
--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -29,85 +29,52 @@
 
 (setq-default custom-enabled-theme 'weyland-yutani)
 (if (display-graphic-p)
-    (setq custom-enabled-theme 'weyland-yutani)
+    (setq custom-enabled-theme 'doom-Iosvkem)
   (setq custom-enabled-theme 'doom-Iosvkem))
 
 
-(defun customize-for-leuven-theme ()
-  (require 'init-themes-customization-for-leuven)
-  (add-hook 'after-init-hook
-            '(lambda ()
-               (zw/customize-general-leuven-theme)
-               (zw/customize-leuven-theme-for-company)
-               (zw/customize-leuven-theme-for-helm)
-               (zw/customize-leuven-theme-for-org)
-               (zw/customize-leuven-theme-for-swiper)
-               (zw/customize-leuven-theme-for-ivy)
-               (zw/customize-leuven-theme-for-modeline)
-               (zw/customize-leuven-theme-for-symbol-overlay))))
-
-(defun customize-for-weyland-theme ()
-  (require 'init-themes-customization-for-weyland)
-  (add-hook 'after-init-hook
-            '(lambda ()
-               (zw/customize-general-weyland-theme)
-               (zw/customize-weyland-theme-for-helm)
-               (zw/customize-weyland-theme-for-ivy)
-               ;; (zw/customize-weyland-theme-for-swiper)
-               (zw/customize-weyland-theme-for-js2)
-               (zw/customize-weyland-theme-for-indent-guide)))
-  
-  (add-hook 'org-mode-hook 'zw/customize-weyland-theme-for-org)
-  (add-hook 'symbol-overlay-mode-hook 'zw/customize-weyland-theme-for-symbol-overlay)
-  (add-hook 'web-mode-hook 'zw/customize-weyland-theme-for-web-mode)
-  (add-hook 'smartparens-mode-hook 'zw/customize-weyland-theme-for-smartparens)
-  (add-hook 'flycheck-mode-hook 'zw/customize-weyland-theme-for-flycheck)
-  
-  (zw/customize-weyland-theme-for-selectrum)
-  (zw/customize-weyland-theme-for-marginalia)
-  (zw/customize-weyland-theme-for-consult)
-  (zw/customize-weyland-theme-for-orderless)
-  
-  (zw/customize-weyland-theme-for-company)
-  (zw/customize-weyland-theme-for-magit))
-
-
-(defun zw/customize-themes ()
+(defun zw/load-themes ()
   (interactive)
   (require 'init-themes-customization)
-  ;; (if (featurep 'company)
-  ;;     (zw/customize-themes-for-company)
-  ;;   (after-load 'company
-  ;;     (zw/customize-themes-for-company)))
-  (zw/customize-theme 'company 'zw/customize-themes-for-company)
   
-
-  )
-
-;; Ensure that themes will be applied even if they have not been customized
-(defun zw/apply-theme ()
-  (interactive)
-  "Forcibly load the themes listed in `custom-enabled-themes'."
-  (setq custom-safe-themes t) ; Don't prompt to confirm theme safety. This 
-  
-  ;; remember to install https://github.com/domtronn/all-the-icons.el
-  (setq doom-themes-treemacs-theme "doom-colors")
-  (doom-themes-treemacs-config)
-
-  (add-hook 'after-init-hook
-            '(lambda ()
-               (load-theme custom-enabled-theme)
-               (zw/customize-themes)))
-  
-  ;; customize themes based on current theme
-  (cond ((eql custom-enabled-theme 'weyland-yutani)
-         (customize-for-weyland-theme))
-        ((eql custom-enabled-theme 'leuven)
-         (customize-for-leuven-theme))))
+  (load-theme custom-enabled-theme)
+  (zw/customize-pkg-with-fn 'company 'zw/customize-themes-for-company))
 
 
-(zw/apply-theme)
+(setq custom-safe-themes t) ; Don't prompt to confirm theme safety. This 
+;; remember to install https://github.com/domtronn/all-the-icons.el
+(setq doom-themes-treemacs-theme "doom-colors")
+(doom-themes-treemacs-config)
+
+(add-hook 'after-init-hook 'zw/load-themes)
 
 
 (provide 'init-themes)
 ;;; init-themes.el ends here
+
+
+;;; previous configurations
+;; (defun customize-for-weyland-theme ()
+;;   (require 'init-themes-customization-for-weyland)
+;;   (add-hook 'after-init-hook
+;;             '(lambda ()
+;;                (zw/customize-general-weyland-theme)
+;;                (zw/customize-weyland-theme-for-helm)
+;;                (zw/customize-weyland-theme-for-ivy)
+;;                ;; (zw/customize-weyland-theme-for-swiper)
+;;                (zw/customize-weyland-theme-for-js2)
+;;                (zw/customize-weyland-theme-for-indent-guide)))
+
+;;   (add-hook 'org-mode-hook 'zw/customize-weyland-theme-for-org)
+;;   (add-hook 'symbol-overlay-mode-hook 'zw/customize-weyland-theme-for-symbol-overlay)
+;;   (add-hook 'web-mode-hook 'zw/customize-weyland-theme-for-web-mode)
+;;   (add-hook 'smartparens-mode-hook 'zw/customize-weyland-theme-for-smartparens)
+;;   (add-hook 'flycheck-mode-hook 'zw/customize-weyland-theme-for-flycheck)
+
+;;   (zw/customize-weyland-theme-for-selectrum)
+;;   (zw/customize-weyland-theme-for-marginalia)
+;;   (zw/customize-weyland-theme-for-consult)
+;;   (zw/customize-weyland-theme-for-orderless)
+
+;;   (zw/customize-weyland-theme-for-company)
+;;   (zw/customize-weyland-theme-for-magit))
