23e26fce6e55e9294283f1711b2a560af43386a7
zwpdbh <hyperion_z@outlook.com>
2021-02-14 10:24:46 +0800

    << customize themes

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -33,32 +33,6 @@
     (setq custom-enabled-theme 'doom-vibrant)
   (setq custom-enabled-theme 'doom-Iosvkem))
 
-;;; previous configurations
-;; (defun customize-for-weyland-theme ()
-;;   (require 'init-themes-customization-for-weyland)
-;;   (add-hook 'after-init-hook
-;;             '(lambda ()
-;;                (zw/customize-general-weyland-theme)
-;;                (zw/customize-weyland-theme-for-helm)
-;;                (zw/customize-weyland-theme-for-ivy)
-;;                ;; (zw/customize-weyland-theme-for-swiper)
-;;                (zw/customize-weyland-theme-for-js2)
-;;                (zw/customize-weyland-theme-for-indent-guide)))
-
-;;   (add-hook 'org-mode-hook 'zw/customize-weyland-theme-for-org)
-;;   (add-hook 'symbol-overlay-mode-hook 'zw/customize-weyland-theme-for-symbol-overlay)
-;;   (add-hook 'web-mode-hook 'zw/customize-weyland-theme-for-web-mode)
-;;   (add-hook 'smartparens-mode-hook 'zw/customize-weyland-theme-for-smartparens)
-;;   (add-hook 'flycheck-mode-hook 'zw/customize-weyland-theme-for-flycheck)
-
-;;   (zw/customize-weyland-theme-for-selectrum)
-;;   (zw/customize-weyland-theme-for-marginalia)
-;;   (zw/customize-weyland-theme-for-consult)
-;;   (zw/customize-weyland-theme-for-orderless)
-
-;;   (zw/customize-weyland-theme-for-company)
-;;   (zw/customize-weyland-theme-for-magit))
-
 
 ;; load theme and do customization for themes
 (defun zw/load-themes ()
