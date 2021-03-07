0535e4d2671ff7376f4004ac595489eba06cf507
zwpdbh <hyperion_z@outlook.com>
2021-03-07 11:18:04 +0800

    <<

--- lisp/init-haskell.el
+++ lisp/init-haskell.el
@@ -23,20 +23,20 @@
   (after-load 'haskell-mode
     ;; .hs is the common extension for haskell file
     ;; use this command to start haskell-model REPL    
-    (define-key haskell-mode-map (kbd "C-c C-c") 'haskell-process-load-file)
+    (define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-file)
     (define-key haskell-mode-map (kbd "C-c C-z") 'haskell-interactive-switch)
     (define-key haskell-mode-map (kbd "C-c C-n C-t") 'haskell-process-do-type)
     (define-key haskell-mode-map (kbd "C-c C-n C-i") 'haskell-process-do-info)
     (define-key haskell-mode-map (kbd "C-c C-n C-c") 'haskell-process-cabal-build)
     (define-key haskell-mode-map (kbd "C-c C-n c") 'haskell-process-cabal)
-    (define-key haskell-mode-map (kbd "C-c C-o") 'haskell-compile))
+    (define-key haskell-mode-map (kbd "C-c C-c") 'haskell-compile))
 
   (after-load 'haskell-cabal
     (define-key haskell-cabal-mode-map (kbd "C-c C-z") 'haskell-interactive-switch)
     (define-key haskell-cabal-mode-map (kbd "C-c C-k") 'haskell-interactive-mode-clear)
     (define-key haskell-cabal-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
     (define-key haskell-cabal-mode-map (kbd "C-c c") 'haskell-process-cabal)
-    (define-key haskell-cabal-mode-map (kbd "C-c C-o") 'haskell-compile)))
+    (define-key haskell-cabal-mode-map (kbd "C-c C-c") 'haskell-compile)))
 
 ;; ;; TODO: install ghc from cabal
 ;; (when (maybe-require-package 'company-ghc)
