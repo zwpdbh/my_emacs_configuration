c05149857c5f885a3ac38b295e9bb1b21c425369
zwpdbh <hyperion_z@outlook.com>
2021-04-01 23:30:15 +0800

    <update org tag alist

--- lisp/init-org-tools.el
+++ lisp/init-org-tools.el
@@ -79,12 +79,11 @@
   ;; enable in markdown, too
   (add-hook 'markdown-mode-hook 'toc-org-mode))
 
-;; ;; Add additional tags shortcut when adding tags through C-c C-q.
-;; (after-load 'org
-;;   (add-to-list 'org-tag-alist '("Lisp" . ?l))
-;;   (add-to-list 'org-tag-alist '("Emacs" . ?e))
-;;   (add-to-list 'org-tag-alist '("TODO" . ?t))
-;;   (add-to-list 'org-tag-alist '("Go" . ?g)))
+;; Add additional tags shortcut when adding tags through C-c C-q.
+(after-load 'org
+  (add-to-list 'org-tag-alist '("Lisp" . ?l))
+  (add-to-list 'org-tag-alist '("Emacs" . ?e))
+  (add-to-list 'org-tag-alist '("TODO" . ?t)))
 
 
 ;; ;; Set the background of org-exported <code> blocks according to theme
