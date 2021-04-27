94baeed7be2979be1aa601613ef6e407fd4a108d
zwpdbh <hyperion_z@outlook.com>
2021-04-27 10:19:18 +0800

    >> test distel

--- lisp/init-erlang.el
+++ lisp/init-erlang.el
@@ -8,35 +8,39 @@
 ;; https://www.lambdacat.com/post-modern-emacs-setup-for-erlang/
 ;; https://seancribbs.com/emacs.d#sec-6-3
 
+;; The OTP emacs mode doesn't come with some of my commonly used files that are Erlang code or terms.
+(add-to-list 'auto-mode-alist '("rebar.config" . erlang-mode))
+(add-to-list 'auto-mode-alist '("rebar.config.script" . erlang-mode))
+(add-to-list 'auto-mode-alist '("app.config" . erlang-mode))
+(add-to-list 'auto-mode-alist '("\\.erlang$" . erlang-mode))
 
-(cond (*win64*
-       (add-to-list 'exec-path "c:/Program Files/erl-23.0/bin")
-       (add-to-list 'load-path "c:/Program Files/erl-23.0/lib/tools-3.4/emacs")
-       (setq erlang-root-dir "c:/Program Files/erl-23.0"))
-      (t
-       (add-to-list 'exec-path "/usr/bin")
-       (add-to-list 'load-path "/usr/lib/erlang/lib/tools-2.11.1/emacs")
-       (add-to-list 'exec-path "/usr/local/bin")
-       (setq erlang-root-dir "/usr/local/otp")))
 
 ;; When Erlang has been installed, it by default contains elisp code for Emacs to use.
 ;; Configure to load those .el files for erlang-mode
-(when (executable-find "erl")
-  (defvar erlang-emacs-dir)
-  (cond (*win64*
-         (setq erlang-emacs-dir "c:/Program Files/erl-23.0/lib/tools-3.4/emacs/")
-         (add-to-list 'load-path erlang-emacs-dir))
-        (t
-         (setq erlang-emacs-dir "/usr/lib/erlang/lib/tools-2.11.1/emacs/")
-         (add-to-list 'load-path erlang-emacs-dir)))
+(cond (*win64*
+       (setq erlang-root-dir "c:/Program Files/erl-23.0/")
+       (setq erlang-man-root-dir (concat erlang-root-dir "man"))
+       (add-to-list 'exec-path (concat erlang-root-dir "bin/"))
+       (add-to-list 'load-path (concat erlang-root-dir "lib/tools-3.4/emacs")))
+      (t
+       (setq erlang-root-dir "/usr/lib/erlang/")
+       (setq erlang-man-root-dir (concat erlang-root-dir "man"))
+       (add-to-list 'exec-path "/usr/local/bin")
+       (add-to-list 'exec-path "/usr/bin")
+       (add-to-list 'load-path (concat erlang-root-dir "lib/tools-2.11.1/emacs"))))
 
-  (when (file-exists-p (concat erlang-emacs-dir "erlang-start.el"))
-    (require 'erlang-start)
-    ;; The OTP emacs mode doesn't come with some of my commonly used files that are Erlang code or terms.
-    (add-to-list 'auto-mode-alist '("rebar.config" . erlang-mode))
-    (add-to-list 'auto-mode-alist '("rebar.config.script" . erlang-mode))
-    (add-to-list 'auto-mode-alist '("app.config" . erlang-mode))
-    (add-to-list 'auto-mode-alist '("\\.erlang$" . erlang-mode))))
+;; for loading https://github.com/massemanet/distel
+(add-to-list 'load-path
+             "~/.emacs.d/site-lisp/distel/elisp")
+
+(when (executable-find "erl")
+  (require 'erlang-start)
+
+  (after-load 'erlang
+    ;; ref: http://alexott.net/en/writings/emacs-devenv/EmacsErlang.html
+    ;; Installation & customization of distel
+    (require 'distel)
+    (distel-setup)))
 
 
 ;; Support LFE(lisp-flavoured-erlang)
@@ -73,6 +77,7 @@
   (after-load 'org
     (defun org-babel-execute:lfe (body params) body)
     (add-to-list 'org-structure-template-alist '("lfe" . "src lfe"))))
+
 
 
 (provide 'init-erlang)
