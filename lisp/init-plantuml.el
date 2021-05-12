1ff49000e53f8ff4bad283e6e791be05c3563e65
zwpdbh <hyperion_z@outlook.com>
2021-05-12 21:14:25 +0800

    <set plantuml output image format as png by default, svg has error

--- lisp/init-plantuml.el
+++ lisp/init-plantuml.el
@@ -1,16 +1,16 @@
-
 (when (maybe-require-package 'plantuml-mode)
+  (add-to-list 'auto-mode-alist '("\\.plantuml\\'" . plantuml-mode))
+  
   (setq plantuml-default-exec-mode 'jar)
   (setq plantuml-jar-path "~/.emacs.d/plantuml.jar")
-  (setq plantuml-output-type "svg")
-  ;; needed by ob-plantuml.el
-  (setq org-plantuml-jar-path "~/.emacs.d/plantuml.jar")
-  (add-to-list 'auto-mode-alist '("\\.plantuml\\'" . plantuml-mode)))
+  (setq plantuml-output-type "png"))
 
 (add-hook 'plantuml-mode-hook '(lambda ()
                                  (setq-local company-backends (add-to-list 'company-backends 'plantuml-complete-symbol))))
 
 (after-load 'org
+  ;; needed by ob-plantuml.el
+  (setq org-plantuml-jar-path "~/.emacs.d/plantuml.jar")
   (add-to-list 'org-structure-template-alist '("plantuml" . "src plantuml")))
 
 (provide 'init-plantuml)
