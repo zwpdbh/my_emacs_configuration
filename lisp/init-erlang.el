956f9573878e4b9b1026351b0e07a62ee14d2776
zwpdbh <hyperion_z@outlook.com>
2021-05-15 00:10:15 +0800

    <

--- lisp/init-erlang.el
+++ lisp/init-erlang.el
@@ -113,7 +113,7 @@
 
 (add-hook 'erlang-shell-mode-hook
           (lambda ()
-            (setq-local company-backends '(company-dabbrev company-keywords company-capf company-files company-dabbrev-code))))
+            (setq-local company-backends '((company-dabbrev company-capf) company-keywords company-files company-dabbrev-code))))
 
 (provide 'init-erlang)
 ;;; init-erlang ends here
