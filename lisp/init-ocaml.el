bd6c5aff356f1c60ee0b337a8ffd2f3cc26046fc
zwpdbh <hyperion_z@outlook.com>
2021-05-30 13:07:19 +0800

    <

--- lisp/init-ocaml.el
+++ lisp/init-ocaml.el
@@ -65,4 +65,40 @@
   (add-to-list 'zw/org-babel-load-language-list '(ocaml . t))
   (add-to-list 'org-structure-template-alist '("ocaml" . "src ocaml :results verbatim")))
 
+
+;;; For ReasonML
+(when (maybe-require-package 'reason-mode)
+  (defun shell-cmd (cmd)
+    "Returns the stdout output of a shell command or nil if the command returned
+   an error"
+    (car (ignore-errors (apply 'process-lines (split-string cmd)))))
+
+  (defun reason-cmd-where (cmd)
+    (let ((where (shell-cmd cmd)))
+      (if (not (string-equal "unknown flag ----where" where))
+          where)))
+
+  (let* ((refmt-bin (or (reason-cmd-where "refmt ----where")
+                        (shell-cmd "which refmt")
+                        (shell-cmd "which bsrefmt")))
+         (merlin-bin (or (reason-cmd-where "ocamlmerlin ----where")
+                         (shell-cmd "which ocamlmerlin")))
+         (merlin-base-dir (when merlin-bin
+                            (replace-regexp-in-string "bin/ocamlmerlin$" "" merlin-bin))))
+    ;; Add merlin.el to the emacs load path and tell emacs where to find ocamlmerlin
+    (when merlin-bin
+      (add-to-list 'load-path (concat merlin-base-dir "share/emacs/site-lisp/"))
+      (setq merlin-command merlin-bin))
+
+    (when refmt-bin
+      (setq refmt-command refmt-bin)))
+
+  (add-hook 'reason-mode-hook
+            (lambda ()
+              ;; remember to comment out merlin-company auto-appending from merlin-company.el which is shipped with merlin
+              (setq-local company-backends (zw/add-to-company-backends 'merlin-company-backend))
+              (setq-local company-backends (zw/delete-from-company-backends 'company-capf))
+              
+              (add-hook 'before-save-hook 'refmt-before-save))))
+
 (provide 'init-ocaml)
