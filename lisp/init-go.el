fd696d0acf63639c8830cc81efe1d67d56c6f50d
zwpdbh <hyperion_z@outlook.com>
2021-06-04 15:16:14 +0800

    < adjust tab-width to 4 for go-mode

--- lisp/init-go.el
+++ lisp/init-go.el
@@ -16,6 +16,7 @@
 
 (add-hook 'go-mode-hook
           '(lambda ()
+             (setq-local tab-width 4)
              (flycheck-mode t)             
              (when (featurep 'flycheck)
                (define-key (current-local-map) (kbd "C-c C-n") 'flycheck-next-error)
