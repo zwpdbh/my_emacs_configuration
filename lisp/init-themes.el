3b9a006b1830ad17684eb36a403cfd6b77bbeb24
zwpdbh <hyperion_z@outlook.com>
2021-06-18 10:52:24 +0800

    <change theme

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -33,7 +33,7 @@
 ;; set default theme
 (setq-default custom-enabled-theme 'weyland-yutani)
 (if (display-graphic-p)
-    (setq custom-enabled-theme 'wombat)
+    (setq custom-enabled-theme 'moe-dark)
   (setq custom-enabled-theme 'doom-Iosvkem))
 
 ;; load theme and do customization for themes
