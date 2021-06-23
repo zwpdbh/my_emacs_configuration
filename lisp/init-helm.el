8ff13521b480c12cec1dc212f278629875c61381
zwpdbh <hyperion_z@outlook.com>
2021-06-23 16:14:56 +0800

    < set key-binding for helm swoop

--- lisp/init-helm.el
+++ lisp/init-helm.el
@@ -1,11 +1,19 @@
 ;; ref: https://github.com/emacsorphanage/helm-swoop
 (when (maybe-require-package 'helm-swoop)
+  (setq helm-swoop-use-fuzzy-match t)
+  (setq helm-swoop-use-line-number-face t)
+  (setq helm-swoop-move-to-line-cycle t)
+  (setq helm-swoop-split-with-multiple-windows t)
+  
+  (setq helm-swoop-pre-input-function
+        (lambda () (thing-at-point 'symbol)))
+ 
   (after-load 'helm-swoop
-    (setq helm-swoop-use-fuzzy-match t)
-    (setq helm-swoop-use-line-number-face t)
-    (setq helm-swoop-move-to-line-cycle t)
-    (setq helm-swoop-split-with-multiple-windows t)
-    (define-key helm-swoop-map (kbd "M-m") 'helm-multi-swoop-current-mode-from-helm-swoop)))
+    (global-set-key (kbd "C-c C-s") 'helm-swoop)
+    (define-key helm-swoop-map (kbd "C-p") 'helm-previous-line)
+    (define-key helm-swoop-map (kbd "C-n") 'helm-next-line)
+    (define-key helm-multi-swoop-map (kbd "C-p") 'helm-previous-line)
+    (define-key helm-multi-swoop-map (kbd "C-n") 'helm-next-line)))
 
 (when (maybe-require-package 'helm-projectile)
   (after-load 'projectile
