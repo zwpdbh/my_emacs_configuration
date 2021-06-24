82ed540f452e2d646650b7f05bb371df7c48be96
zwpdbh <hyperion_z@outlook.com>
2021-06-24 21:46:50 +0800

    <no need completation for shell

--- lisp/init-shell.el
+++ lisp/init-shell.el
@@ -1,13 +1,7 @@
 ;; prevent backspace from deleting my shell prompt
 (setq comint-prompt-read-only t)
 
-(when (maybe-require-package 'bash-completion)
-  (autoload 'bash-completion-dynamic-complete
-    "bash-completion"
-    "BASH completion hook")
-  (add-hook 'shell-dynamic-complete-functions
-            'bash-completion-dynamic-complete))
-
+;; It is used to customize shell settings during shell-mode
 (defun zw/set-bash-profile ()
   (interactive)
   (progn
