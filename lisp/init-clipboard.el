919d8fcde6e6702fbf5c37e287e2dde26cff9e80
zwpdbh <hyperion_z@outlook.com>
2021-06-24 10:18:42 +0800

    <<fix xclip problem

--- lisp/init-clipboard.el
+++ lisp/init-clipboard.el
@@ -5,6 +5,7 @@
 ;; So `C-y' could paste from clipbord if you are NOT using emacs-nox
 ;; I only use `paste-from-x-clipboard', not `C-y'.
 
+;; Require ~/.emacs.d/site-lisp/xclip/xclip.el
 ;; This two are must set
 (autoload 'xclip-set-selection "xclip" "" t)
 (autoload 'xclip-get-selection "xclip" "" t)
