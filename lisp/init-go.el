ad527ced02faef5d78a0d6380f3925fd9be5f3cc
zwpdbh <hyperion_z@outlook.com>
2021-06-24 08:53:36 +0800

    <enhance a little go

--- lisp/init-go.el
+++ lisp/init-go.el
@@ -18,32 +18,39 @@
     (add-to-list 'zw/org-babel-load-language-list '(go . t))))
 
 
+(defun customize-compile-for-go ()
+  (when (not (string-match "go" compile-command))
+    (set (make-local-variable 'compile-command)
+         "go build -v && go test -v && go vet")))
+
 (after-load 'go-mode
   ;; disable annoying *Gofmt Errors* buffer
   (setq gofmt-show-errors nil)
   (define-key go-mode-map (kbd "C-c C-c") #'go-run))
 
 (add-hook 'go-mode-hook
-          '(lambda ()             
-             (setq-local tab-width 4)
-             
-             (flycheck-mode t)             
-             (when (featurep 'flycheck)
-               (define-key (current-local-map) (kbd "C-c C-n") 'flycheck-next-error)
-               (define-key (current-local-map) (kbd "C-c C-p") 'flycheck-previous-error))
-             
-             (add-hook 'before-save-hook 'gofmt-before-save nil 'local)
-             ;; Godef, lets you quickly jump around the code
-             ;; Install it by: go get github.com/rogpeppe/godef
-             (if (locate-file "godef" exec-path)
-                 (progn
-                   (define-key go-mode-map (kbd "M-.") 'godef-jump)
-                   (define-key go-mode-map (kbd "M-,") 'pop-tag-mark)
-                   (define-key go-mode-map (kbd "M-/") 'zw/counsel-etags-grep-at-point))
-               (progn
-                 (define-key go-mode-map (kbd "M-.") 'counsel-etags-find-tag-at-point)
-                 (define-key go-mode-map (kbd "M-,") 'pop-tag-mark)
-                 (define-key go-mode-map (kbd "M-/") 'zw/counsel-etags-grep-at-point)))))
+          (lambda ()             
+            (setq-local tab-width 4)
+            
+            (flycheck-mode t)             
+            (when (featurep 'flycheck)
+              (define-key (current-local-map) (kbd "C-c C-n") 'flycheck-next-error)
+              (define-key (current-local-map) (kbd "C-c C-p") 'flycheck-previous-error))
+            
+            (add-hook 'before-save-hook 'gofmt-before-save nil 'local)
+            ;; Godef, lets you quickly jump around the code
+            ;; Install it by: go get github.com/rogpeppe/godef
+            (if (locate-file "godef" exec-path)
+                (progn
+                  (define-key go-mode-map (kbd "M-.") 'godef-jump)
+                  (define-key go-mode-map (kbd "M-,") 'pop-tag-mark)
+                  (define-key go-mode-map (kbd "M-/") 'zw/counsel-etags-grep-at-point))
+              (progn
+                (define-key go-mode-map (kbd "M-.") 'counsel-etags-find-tag-at-point)
+                (define-key go-mode-map (kbd "M-,") 'pop-tag-mark)
+                (define-key go-mode-map (kbd "M-/") 'zw/counsel-etags-grep-at-point)))
+
+            (customize-compile-for-go)))
 
 
 (provide 'init-go)
