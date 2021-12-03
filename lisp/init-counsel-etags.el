e9ea242a7c3a2979e47c2baf1a9543e9c56ca065
Wei Zhao <zhaowei@microsoft.com>
2021-12-03 10:12:03 +0800

    <use ctags to handle csharp

--- lisp/init-counsel-etags.el
+++ lisp/init-counsel-etags.el
@@ -4,68 +4,72 @@
 (use-package counsel
   :ensure t)
 
-(when (maybe-require-package 'counsel-etags)
-  ;; (require 'counsel-etags)
-  
-  ;; set Exuberant Ctags or Universal Ctags
-  ;; On Ubuntu, we could install Universal Ctags by
-  ;; sudo snap install universal-ctags, then check it by universal-ctags --version
-  ;; Notice: There may be a ctags which is actually etags.
-  ;; That etags (which is /usr/local/bin/ctags) is probably installed from Emacs (maybe by native compile?)
-  ;; The problem is it may shadow the real ctags or universal-ctags.
-  ;; So, the counsel-etags failed to find available ctags program
-  (if *win64*
-      (add-to-list 'exec-path "c:/ProgramData/chocolatey/bin/")
+(when (executable-find "ctags")
+  (when (maybe-require-package 'counsel-etags)
+    ;; (require 'counsel-etags)
+
+    ;; NOTICE!!!
+    ;; Set Exuberant Ctags or Universal Ctags
+    ;; On Ubuntu, we could install Universal Ctags by sudo snap install universal-ctags, then check it by universal-ctags --version.
+    ;; Notice: There may be a ctags which is actually etags.
+    ;; That etags (which is /usr/local/bin/ctags) is probably installed from Emacs (maybe by native compile?)
+    ;; The problem is it may shadow the real ctags or universal-ctags.
+    ;; So, the counsel-etags failed to find available ctags program
+    ;; On Windows, we install Universal Ctags by choco install universal-ctags
+    
+    (if *win64*
+        (add-to-list 'exec-path "c:/ProgramData/chocolatey/bin/")
       (add-to-list 'exec-path "/snap/bin/"))
-  
-  (defun zw/counsel-etags-grep-at-point ()
-    (interactive)
-    (counsel-etags-grep (counsel-etags-tagname-at-point) nil nil t))
+    
+    (defun zw/counsel-etags-grep-at-point ()
+      (interactive)
+      (counsel-etags-grep (counsel-etags-tagname-at-point) nil nil t))
+
+    ;; adjust key-bindings for counsel-etags
+    (defun zw/counsel-etags-key-bindings ()
+      (interactive)
+      (define-key (current-local-map) (kbd "M-.") 'counsel-etags-find-tag-at-point)
+      (define-key (current-local-map) (kbd "M-/") 'zw/counsel-etags-grep-at-point))
+
+    (setq zw/use-counsel-etags-modes '(yaml-mode
+                                       json-mode
+                                       elisp-mode))
+
+    (dolist (each-mode zw/use-counsel-etags-modes)
+      (add-hook (intern (format "%s-hook" each-mode))
+                'zw/counsel-etags-setup)))
 
 
-  ;; adjust key-bindings for counsel-etags
-  (defun zw/counsel-etags-key-bindings ()
-    (interactive)
-    (define-key (current-local-map) (kbd "M-.") 'counsel-etags-find-tag-at-point)
-    (define-key (current-local-map) (kbd "M-/") 'zw/counsel-etags-grep-at-point))
+  ;; Ignore directories and files
+  (after-load 'counsel-etags
+    (setq tags-add-tables t)
+    (setq imenu-create-index-function 'counsel-etags-imenu-default-create-index-function)
 
-  (setq zw/use-counsel-etags-modes '(yaml-mode
-                                     json-mode
-                                     elisp-mode))
-  ;; Setup auto update now
-  (defun zw/counsel-etags-setup ()
-    (interactive)
+    ;; Don't ask before rereading the TAGS files if they have changed
+    (setq tags-revert-without-query t)
+    ;; Don't warn when TAGS files are large
+    (setq large-file-warning-threshold nil)
+    (setq counsel-etags-update-interval 30)
+    
+    ;; counsel-etags-ignore-directories does NOT support wildcast
+    (add-to-list 'counsel-etags-ignore-directories "dist")
+    (add-to-list 'counsel-etags-ignore-directories "build_clang")
+    (add-to-list 'counsel-etags-ignore-directories "build")
+    (add-to-list 'counsel-etags-ignore-directories "node_modules")
+    
+    ;; counsel-etags-ignore-filenames supports wildcast
+    (add-to-list 'counsel-etags-ignore-filenames "TAGS")
+    (add-to-list 'counsel-etags-ignore-filenames "package-lock.json")
+    (add-to-list 'counsel-etags-ignore-filenames "#*")
+    (add-to-list 'counsel-etags-ignore-filenames "#*.*#")
+    (add-to-list 'counsel-etags-ignore-filenames "*.*#")))
+
+
+(defun zw/counsel-etags-setup ()
+  (interactive)
+  (when (fboundp 'zw/counsel-etags-key-bindings)
     (zw/counsel-etags-key-bindings)
     (add-hook 'after-save-hook
-              'counsel-etags-virtual-update-tags 'append 'local))
-
-  (dolist (each-mode zw/use-counsel-etags-modes)
-    (add-hook (intern (format "%s-hook" each-mode))
-              'zw/counsel-etags-setup)))
-
-
-;; Ignore directories and files
-(with-eval-after-load 'counsel-etags
-  (setq tags-add-tables t)
-  (setq imenu-create-index-function 'counsel-etags-imenu-default-create-index-function)
-
-  ;; Don't ask before rereading the TAGS files if they have changed
-  (setq tags-revert-without-query t)
-  ;; Don't warn when TAGS files are large
-  (setq large-file-warning-threshold nil)
-  (setq counsel-etags-update-interval 30)
-  
-  ;; counsel-etags-ignore-directories does NOT support wildcast
-  (add-to-list 'counsel-etags-ignore-directories "dist")
-  (add-to-list 'counsel-etags-ignore-directories "build_clang")
-  (add-to-list 'counsel-etags-ignore-directories "build")
-  (add-to-list 'counsel-etags-ignore-directories "node_modules")
-  
-  ;; counsel-etags-ignore-filenames supports wildcast
-  (add-to-list 'counsel-etags-ignore-filenames "TAGS")
-  (add-to-list 'counsel-etags-ignore-filenames "package-lock.json")
-  (add-to-list 'counsel-etags-ignore-filenames "#*")
-  (add-to-list 'counsel-etags-ignore-filenames "#*.*#")
-  (add-to-list 'counsel-etags-ignore-filenames "*.*#"))
+              'counsel-etags-virtual-update-tags 'append 'local)))
 
 (provide 'init-counsel-etags)
