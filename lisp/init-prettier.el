b5beaaf9d5e5e625b5f44f78cb53bd6275c6aa63
Wei Zhao <zhaowei@microsoft.com>
2022-02-14 15:48:31 +0800

    <<

--- lisp/init-prettier.el
+++ lisp/init-prettier.el
@@ -5,11 +5,13 @@
 ;; prettier is the tool used to format code
 ;; The prettier(prettier.el) Emacs package reformats your code by running Prettier with minimal overhead, by request or transparently on file save.
 (when (maybe-require-package 'prettier)
-  ;; format code for web-mode before save 
-  (add-hook 'web-mode-hook
-            '(lambda ()
-               (add-hook 'before-save-hook
-                         'prettier-prettify nil 'local)))
+  ;; format code for web-mode before save
+  (when (fboundp 'prettier)
+    (add-hook 'web-mode-hook
+              '(lambda ()
+                 (add-hook 'before-save-hook
+                           'prettier-prettify nil 'local))))
+
 
   (add-hook 'sgml-mode-hook
             '(lambda ()
