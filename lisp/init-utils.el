ca504a56123beb274eb261a595028084b1520162
zwpdbh <hyperion_z@outlook.com>
2022-03-04 22:08:14 +0800

    <tmp

--- lisp/init-utils.el
+++ lisp/init-utils.el
@@ -88,5 +88,19 @@
       (browse-url (concat "file://" file-name)))))
 
 
+
+;; To solve issue: "^M" appeared at the end of file
+(setq-default buffer-file-coding-system 'utf-8-unix)
+(set-buffer-file-coding-system 'utf-8-unix)
+
+(defun zw/delete-annoying-carrage-returns ()
+  (interactive)
+  (save-excursion
+    (goto-char 0)
+    (while (search-forward "\r" nil :noerror)
+      (replace-match ""))))
+
+
+
 (provide 'init-utils)
 ;;; init-utils.el ends here
