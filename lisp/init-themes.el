b41ac16168e5ffbe3ec80ea676ea58f4aeec97b8
zwpdbh <hyperion_z@outlook.com>
2022-03-04 21:32:13 +0800

    <<

--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -21,7 +21,7 @@
 ;; ===set default theme
 (setq-default custom-enabled-theme 'doom-gruvbox)
 (unless (display-graphic-p)
-  (setq custom-enabled-theme 'doom-Iosvkem))
+  (setq custom-enabled-theme 'wombat))
 
 
 ;; load theme and do customization for themes
