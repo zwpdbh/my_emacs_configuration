9055d4556a92fbb985bac7596a836695a4db0a00
zwpdbh <hyperion_z@outlook.com>
2022-03-04 20:13:47 +0800

    <

--- lisp/init-yasnippet.el
+++ lisp/init-yasnippet.el
@@ -5,10 +5,9 @@
   (when (maybe-require-package 'yasnippet-snippets)))
 
 (defun yasnippet-generic-setup-for-mode-hook ()
-  (unless (is-buffer-file-temp)
-    (yas-minor-mode 1)
-    (when (featurep 'company)
-      (setq-local company-backends (zw/add-to-company-backends 'company-yasnippet)))))
+  (yas-minor-mode 1)
+  (when (featurep 'company)
+    (setq-local company-backends (zw/add-to-company-backends 'company-yasnippet))))
 
 (add-hook 'prog-mode-hook 'yasnippet-generic-setup-for-mode-hook)
 (add-hook 'text-mode-hook 'yasnippet-generic-setup-for-mode-hook)
