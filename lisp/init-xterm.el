431b07d71791c36b06d94db6d4cdb6c2a3b90ab5
zwpdbh <hyperion_z@outlook.com>
2022-03-12 22:51:05 +0800

    <

--- lisp/init-xterm.el
+++ lisp/init-xterm.el
@@ -2,7 +2,7 @@
 ;;; Commentary:
 ;;; Code:
 
-(require 'init-frame-hooks)
+;; (require 'init-frame-hooks)
 
 (global-set-key [mouse-4] (lambda () (interactive) (scroll-down 1)))
 (global-set-key [mouse-5] (lambda () (interactive) (scroll-up 1)))
