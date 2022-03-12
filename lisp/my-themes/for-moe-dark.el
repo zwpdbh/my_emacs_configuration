a40553d9bb5b133e81ea81b89f18c49440d381e0
zwpdbh <hyperion_z@outlook.com>
2022-03-12 16:41:07 +0800

    <

--- lisp/my-themes/for-moe-dark.el
+++ lisp/my-themes/for-moe-dark.el
@@ -1,2 +1,10 @@
 (set-face-attribute 'font-lock-comment-face nil
                     :slant 'normal)
+
+(set-face-attribute 'consult-preview-line nil
+                    :foreground "Yellow"
+                    :background (face-background 'default nil t))
+
+(set-face-attribute 'consult-preview-cursor nil
+                    :foreground (face-foreground 'default nil t)
+                    :background (face-background 'default nil t))
