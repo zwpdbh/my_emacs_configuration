c21c296b3440dabd6287b5f2ef0426fd5951a3e1
Wei Zhao <zhaowei@microsoft.com>
2022-03-14 12:26:08 +0800

    <support powershell-mode

--- lisp/init-powershell.el
+++ lisp/init-powershell.el
@@ -3,5 +3,6 @@
     ;; create our dummy one 
     (defun org-babel-execute:powershell (body params) body)
     ;; notice: it is case sensitive
+    (add-to-list 'org-structure-template-alist '("ps" . "src powershell"))
     (add-to-list 'org-structure-template-alist '("powershell" . "src powershell"))))
 (provide 'init-powershell)
