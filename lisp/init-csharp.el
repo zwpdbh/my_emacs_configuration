6b9745930714e6346b6cfe669a4acf799270c6bd
zwpdbh <hyperion_z@outlook.com>
2022-03-14 02:20:01 -0700

    <adjust csharp mode

--- lisp/init-csharp.el
+++ lisp/init-csharp.el
@@ -1,38 +1,40 @@
 (when (executable-find "dotnet")
-  (when (maybe-require-package 'csharp-mode)
-    (when (maybe-require-package 'omnisharp)
-      ;; After the package is installed you’ll need to install the omnisharp server by running M-x omnisharp-install-server
-      (defun my-csharp-mode-hook ()
-        (omnisharp-mode t)
-        (company-mode t)
-        (flycheck-mode -1)
-        (setq-local company-backends (zw/add-to-company-backends 'company-omnisharp))
-        (setq indent-tabs-mode nil)
-        (setq c-syntactic-indentation t)
-        (c-set-style "ellemtel")
-        (setq c-basic-offset 4)
-        (setq truncate-lines t)
-        (setq tab-width 4)
+  (maybe-require-package 'csharp-mode)
+  ;; After the package is installed you’ll need to install the omnisharp server by
+  ;; running M-x omnisharp-install-server
+  (maybe-require-package 'omnisharp)
 
-        (local-set-key (kbd "C-c r r") 'omnisharp-run-code-action-refactoring)
-        (local-set-key (kbd "C-c C-c") 'recompile)
-
-        (if (fboundp 'omnisharp-find-implementations)
-            (progn
-              (define-key csharp-mode-map (kbd "M-.") 'omnisharp-find-implementations)
-              (if (fboundp 'zw/consult-ripgrep-at-point)
-                  (define-key csharp-mode-map (kbd "M-/") 'zw/consult-ripgrep-at-point)
-                (define-key csharp-mode-map (kbd "M-/") 'omnisharp-find-usages)))
-          (zw/counsel-etags-setup))))
+  (defun my-csharp-mode-hook ()
+    (setq-local indent-tabs-mode nil)
+    (setq-local c-syntactic-indentation t)
+    (setq-local c-basic-offset 4)
+    (setq-local truncate-lines t)
+    (setq-local tab-width 4)
     
-    (add-hook 'csharp-mode-hook 'my-csharp-mode-hook)
-    (add-to-list 'auto-mode-alist '("\\.cshtml\\'" . web-mode))
+    (c-set-style "ellemtel")
+    (flycheck-mode -1)
 
-    (after-load 'org
-      ;; Uncomment one of the following to be able to execute csharp code block.
-      ;; (defun org-babel-execute:csharp (body params) body) ; Defun dummy function to execute.
-      ;; (add-to-list 'zw/org-babel-load-language-list '(csharp . t)) ; Only enable this if you have ob-csharp
-      (add-to-list 'org-structure-template-alist '("cs" . "src csharp"))
-      (add-to-list 'org-structure-template-alist '("csharp" . "src csharp")))))
+    (local-set-key (kbd "C-c r r") 'omnisharp-run-code-action-refactoring)
+    (local-set-key (kbd "C-c C-c") 'recompile)
+
+    (if (fboundp 'omnisharp-find-implementations)
+        (progn
+          (omnisharp-mode t)
+          (setq-local company-backends (zw/add-to-company-backends 'company-omnisharp))
+          (define-key csharp-mode-map (kbd "M-.") 'omnisharp-find-implementations)
+          (if (fboundp 'zw/consult-ripgrep-at-point)
+              (define-key csharp-mode-map (kbd "M-/") 'zw/consult-ripgrep-at-point)
+            (define-key csharp-mode-map (kbd "M-/") 'omnisharp-find-usages)))
+      (zw/counsel-etags-setup)))
+
+  (add-hook 'csharp-mode-hook 'my-csharp-mode-hook)
+  (add-to-list 'auto-mode-alist '("\\.cshtml\\'" . web-mode))
+
+  (after-load 'org
+    ;; Uncomment one of the following to be able to execute csharp code block.
+    ;; (defun org-babel-execute:csharp (body params) body) ; Defun dummy function to execute.
+    ;; (add-to-list 'zw/org-babel-load-language-list '(csharp . t)) ; Only enable this if you have ob-csharp
+    (add-to-list 'org-structure-template-alist '("cs" . "src csharp"))
+    (add-to-list 'org-structure-template-alist '("csharp" . "src csharp"))))
 
 (provide 'init-csharp)
