77cba1e12a5ef83f43edf488ddd1c25a02722979
zwpdbh <hyperion_z@outlook.com>
2022-03-23 11:16:31 +0800

    <load xclip from site-lisp

--- lisp/init-clipboard.el
+++ lisp/init-clipboard.el
@@ -6,6 +6,8 @@
 ;; I only use `paste-from-x-clipboard', not `C-y'.
 
 ;; Require ~/.emacs.d/site-lisp/xclip/xclip.el
+(load-file "~/.emacs.d/site-lisp/xclip/xclip.el")
+
 ;; This two are must set
 (autoload 'xclip-set-selection "xclip" "" t)
 (autoload 'xclip-get-selection "xclip" "" t)
