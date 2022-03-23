e43415a05c404e6f38fb19f5d359092326eeb20b
zwpdbh <hyperion_z@outlook.com>
2022-03-23 11:23:40 +0800

    <adjust when to load site-lisp

--- lisp/init-clipboard.el
+++ lisp/init-clipboard.el
@@ -6,7 +6,8 @@
 ;; I only use `paste-from-x-clipboard', not `C-y'.
 
 ;; Require ~/.emacs.d/site-lisp/xclip/xclip.el
-(load-file "~/.emacs.d/site-lisp/xclip/xclip.el")
+(add-hook 'after-init-hook '(lambda ()
+                              (load-file "~/.emacs.d/site-lisp/xclip/xclip.el")))
 
 ;; This two are must set
 (autoload 'xclip-set-selection "xclip" "" t)
